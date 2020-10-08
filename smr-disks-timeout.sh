baab59abe27cbaedd15ef63aa9716fb25ba466a7
root <root@backupss>
2020-10-08 21:30:55 +0200

    xx

--- smr-disks-timeout.sh
+++ smr-disks-timeout.sh
@@ -3,6 +3,10 @@
 # v.1.1 - 2020.10.08 - showing timeouts before the setting them
 # v.1.0 - 2020.10.04 - added timeout variable and increased value of it from 180 to 300s
 
+# good reading: https://raid.wiki.kernel.org/index.php/Timeout_Mismatch
+# "In 2019, a new technology called shingled magnetic recording (SMR) started becoming mainstream"
+
+
 timeout=600
 
 for p in {a..z} ; do 
