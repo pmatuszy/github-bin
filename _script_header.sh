ca77b1f1d48321a75880fb0d88cef5ee59173442
root <root@backupche>
2020-10-08 21:13:03 +0200

    xx

---
+++ _script_header.sh
@@ -0,0 +1,31 @@
+# 2020.09.15 - v. 0.2 - initial release
+# 2020.09.15 - v. 0.1 - initial release
+
+# if we are run non-interactively - do not set the terminal title
+
+export tcScrTitleStart="\ek"
+export tcScrTitleEnd="\e\134"
+
+tty 2>&1 >/dev/null
+if (( $? == 0 )); then
+  echo -ne "\033]0;`hostname` - $0\007";\
+fi
+
+if [ ! -z $STY ]; then    # checking if we are running within screen
+  # I am setting the screen window title to the script name
+  echo -ne "${tcScrTitleStart}${0}${tcScrTitleEnd}"
+fi
+
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+ echo
+ echo "** Trapped CTRL-C - cleaning up...."
+ echo
+ if [ ! -zwhy $STY ]; then    # checking if we are running within screen
+    # I am setting the screen window title to the script name
+    echo -ne "${tcScrTitleStart}${0}${tcScrTitleEnd}"
+ fi
+ exit
+}
