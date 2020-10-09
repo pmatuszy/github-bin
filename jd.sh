fa338d830d4b2aee5254e5c3d1966ca301f3f6a6
Paul G. Matuszyk <58336955+pmatuszy@users.noreply.github.com>
2020-10-09 11:27:04 +0200

    Update jd.sh

--- jd.sh
+++ jd.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+# 2020.10.09 - v. 0.2 - small cosmetic modifications
+# 2020.0x.xx - v. 0.1 - initial release (date unknown)
+
 if [ ! -h /etc/os-release ] ; then
-  echo nie wiem co to za OS - wychodze
-  echo nie wiem co to za OS - wychodze
-  echo nie wiem co to za OS - wychodze
+  echo
+  echo "nie wiem co to za OS - wychodze..."
   echo
   exit 1
 fi
