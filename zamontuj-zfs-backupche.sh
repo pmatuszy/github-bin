778fb36ac45e65b2ccde2c88bc52b917517c995c
Paul G. Matuszyk <58336955+pmatuszy@users.noreply.github.com>
2020-10-09 11:29:24 +0200

    Update zamontuj-zfs-backupche.sh

--- zamontuj-zfs-backupche.sh
+++ zamontuj-zfs-backupche.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
+# 2020.10.09 - v. 0.2 - small cosmetic changes
+# 2020.0x.xx - v. 0.1 - initial release (date unknown)
+
 jd.sh
 
-echo nacisnij ENTER
+echo "nacisnij <ENTER>"
 read r
 
 dmesg
 
-echo nacisnij ENTER
+echo "nacisnij <ENTER>"
 read r
-
 
 zpool export zfs_usb 2>/dev/null
 
@@ -20,5 +23,3 @@ zfs mount -a
 df -h
 
 zfs set sharenfs="rw=@192.168.200.138/32" zfs_usb/worek/podsync-hdd
-
-
