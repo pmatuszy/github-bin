59ea8042b414f2575db2762b10772de9d1e0d7ae
Paul G. Matuszyk <58336955+pmatuszy@users.noreply.github.com>
2020-10-09 11:28:25 +0200

    Update zamontuj-encrypted.sh

--- zamontuj-encrypted.sh
+++ zamontuj-encrypted.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# 2020.0x.xx - v. 0.1 - initial release (date unknown)
+
 zpool export zfs_encrypted_file 2>/dev/null
 zpool import -d /encrypted.zfs -l -a
 
@@ -6,4 +9,3 @@ zpool status -v
 zfs mount zfs_encrypted_file/encrypted
 
 df -h /encrypted
-
