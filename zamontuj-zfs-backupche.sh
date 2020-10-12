635f93cf3c27a18622d293127f19ba3455f65057
root <root@backupche>
2020-10-12 23:36:09 +0200

    xx

--- zamontuj-zfs-backupche.sh
+++ zamontuj-zfs-backupche.sh
@@ -22,6 +22,7 @@ zfs mount -a
 
 df -h
 
-zfs set sharenfs="rw=@192.168.200.138/32" zfs_usb/worek/podsync-hdd
+zfs set sharenfs="rw=@192.168.200.138/32" zfs-raid1/podsync-hdd
 
 mount -o bind /mnt/zfs-raid1/replication1/rclone_user /rclone-jail/storage-master/replication1
+mount -o bind /mnt/zfs-raid1/backup1/rclone_user /rclone-jail/storage-master/backup1
