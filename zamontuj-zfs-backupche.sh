95b97dbe2a7cd4da22ff279693b93a82dfd5744a
root <root@backupche>
2020-10-13 00:20:33 +0200

    xx

--- zamontuj-zfs-backupche.sh
+++ zamontuj-zfs-backupche.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# 2020.10.13 - v. 0.3 - exporting all zpools and then importing them
 # 2020.10.09 - v. 0.2 - small cosmetic changes
 # 2020.0x.xx - v. 0.1 - initial release (date unknown)
 
@@ -12,9 +13,13 @@ dmesg
 echo "nacisnij <ENTER>"
 read r
 
-zpool export zfs_usb 2>/dev/null
+set +x
+zpool export zfs_usb   2>/dev/null
+zpool export zfs-raid1 2>/dev/null
 
 zpool import -d /dev/disk/by-id -l -a
+
+set -x
 
 zpool status -v
 
@@ -22,7 +27,7 @@ zfs mount -a
 
 df -h
 
-zfs set sharenfs="rw=@192.168.200.138/32" zfs-raid1/podsync-hdd
+# zfs set sharenfs="rw=@192.168.200.138/32,rw=192.168.200.109/32" zfs-raid1/podsync-hdd
 
 mount -o bind /mnt/zfs-raid1/replication1/rclone_user /rclone-jail/storage-master/replication1
 mount -o bind /mnt/zfs-raid1/backup1/rclone_user /rclone-jail/storage-master/backup1
