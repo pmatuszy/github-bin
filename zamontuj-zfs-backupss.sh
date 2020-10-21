085998394cbc24267e44ad161e2f993da7cc4f73
Paul G. Matuszyk <58336955+pmatuszy@users.noreply.github.com>
2020-10-21 10:13:42 +0200

    Update zamontuj-zfs-backupss.sh

--- zamontuj-zfs-backupss.sh
+++ zamontuj-zfs-backupss.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# 2020.10.21 - v. 0.3 - added comment about import with rewind
 # 2020.10.09 - v. 0.2 - small cosmetic changes
 # 2020.0x.xx - v. 0.1 - initial release (date unknown)
+
+# DO NOT TRY IT IN PRODUCTION. USE AT YOUR OWN RISK!
+# zpool import -FX mypool where options mean:
+# * -F Attempt rewind if necessary.
+# * -X Turn on extreme rewind.
+# * -T Specify a starting txg to use for import. This option is intentionally undocumented option for testing purposes.
 
 jd.sh
 
