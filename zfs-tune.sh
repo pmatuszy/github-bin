2e4a423783744d561c9544496f468b64adf3dfe5
root <root@backupche>
2020-10-26 18:29:39 +0100

    pgm

---
+++ zfs-tune.sh
@@ -0,0 +1,20 @@
+# 2020.10.26 - v. 0.1 - initial release
+
+
+echo cat /sys/module/zfs/parameters/zfs_prefetch_disable
+cat /sys/module/zfs/parameters/zfs_prefetch_disable
+echo 1 >/sys/module/zfs/parameters/zfs_prefetch_disable
+
+echo cat /sys/module/zfs/parameters/zfs_prefetch_disable
+cat /sys/module/zfs/parameters/zfs_prefetch_disable
+
+###############################################################
+echo
+echo
+echo cat /sys/module/zfs/parameters/zfs_nocacheflush
+cat /sys/module/zfs/parameters/zfs_nocacheflush
+echo 1 >/sys/module/zfs/parameters/zfs_nocacheflush
+
+echo cat /sys/module/zfs/parameters/zfs_nocacheflush
+cat /sys/module/zfs/parameters/zfs_nocacheflush
+
