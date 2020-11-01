b1146519254c01c1f208138697152cf727c19a2a
root <root@backupche>
2020-11-01 21:14:05 +0100

    pgm

---
+++ zamontuj-encrypted-backupche.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 2020.0x.xx - v. 0.1 - initial release (date unknown)
+
+zpool export zfs_encrypted_file 2>/dev/null
+zpool import -d /encrypted.zfs -l -a
+
+zpool status -v
+
+zfs mount zfs_encrypted_file/encrypted
+
+df -h /encrypted
+
+cryptsetup luksOpen /dev/vg_crypto/lv_do_luksa luks-on-lv
+mount -o noatime/dev/mapper/luks-on-lv /mnt/luks-raid1
