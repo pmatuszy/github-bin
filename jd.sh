003009d0b5dc2a879eee3035659fe3b56b1ac41b
root <root@backupche>
2020-11-27 10:17:10 +0100

    "new push from backupche @ 2020.11.27 10:17:10"

--- jd.sh
+++ jd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# 2020.11.27 - v. 0.3 - added printing of the disk serial numbers
 # 2020.10.09 - v. 0.2 - small cosmetic modifications
 # 2020.0x.xx - v. 0.1 - initial release (date unknown)
 
@@ -9,23 +10,28 @@ if [ ! -h /etc/os-release ] ; then
   exit 1
 fi
 
-
-
 if [[ `cat /etc/os-release |grep -qi centos ; echo $?` == 0 ]] ; then
   echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   echo "~~~~~~~~~~~~~ CENTOS ~~~~~~~~~~~~~"
   echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  fdisk -l|grep 'Disk /dev'|egrep -v 'mapper|/md|/ram|mmcb|/dev/loop'|sort
+  for p in `fdisk -l|grep 'Disk /dev'|egrep -v 'mapper|/md|/ram|mmcb|/dev/loop'|sort|tr -d :`;do
+     echo `gdisk -l $p |grep "Disk /dev"` "   " `hdparm -I $p | grep 'Serial\ Number'`
+  done
+
   echo
   fdisk -l|grep 'Disk /dev'|egrep -v 'mapper|/md|/ram|mmcb|/dev/loop'|sort | wc -l
 else
   echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   echo "~~~~~~~~~~~~~ PI  ~~~~~~~~~~~~~"
   echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  fdisk -l|grep 'Disk /dev'|egrep -v 'mapper|/md|/ram|mmcb|/dev/loop'|sort
+  
+#  fdisk -l|grep 'Disk /dev'|egrep -v 'mapper|/md|/ram|mmcb|/dev/loop'|sort
+  for p in `fdisk -l|grep 'Disk /dev'|egrep -v 'mapper|/md|/ram|mmcb|/dev/loop'|awk '{print $2}'|sort|tr -d :`;do
+     echo `gdisk -l $p |grep "Disk /dev"` "   " `hdparm -I $p | grep 'Serial\ Number'`
+  done
+
   echo
   fdisk -l|grep 'Disk /dev'|egrep -v 'mapper|/md|/ram|mmcb|/dev/loop'|sort | wc -l
 fi
 
 echo
-
