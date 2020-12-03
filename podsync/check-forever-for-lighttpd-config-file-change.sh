69f036824279a15b9ad8c03ca56bf40e893acd98
root <root@rpi4ch>
2020-12-03 17:42:51 +0100

    "new push from podsync @ 2020.12.03 17:42:51"

---
+++ podsync/check-forever-for-lighttpd-config-file-change.sh
@@ -0,0 +1,26 @@
+# 2020.12.03 - v. 0.1 - initial release
+
+plik=/etc/lighttpd/lighttpd.conf
+plik_template=/etc/lighttpd/lighttpd.conf.dobry-dziala
+
+
+if [ ! -f ${plik} ]; then
+  echo "plik $plik nie istnieje - wychodze...."
+  exit 1
+fi
+if [ ! -f ${plik_template} ]; then
+  echo "plik $plik_template nie istnieje - wychodze...."
+  exit 2
+fi
+
+opoznienie=1    # opoznienie w sekundach
+
+while : ; do
+    inotifywait -q -e modify ${plik}
+    echo wyszedlem
+    sleep $opoznienie
+    cp ${plik_template} ${plik}
+    systemctl restart lighttpd 
+    echo "plik ${plik} zostal zmodyfikowany, ale przywrocilem domyslna konfiguracje, hehe" | strings | /usr/bin/mailx -s " [ `hostname` ] modifykacja pliku ${plik} zostala odwrocona" matuszyk+`hostname`@matuszyk.com
+    echo "plik ${plik} zostal zmodyfikowany, ale przywrocilem domyslna konfiguracje, hehe" | strings | /usr/bin/mailx -s " [ `hostname` ] modifykacja pliku ${plik} zostala odwrocona" marcin.kozak@fractum.pl 
+done
