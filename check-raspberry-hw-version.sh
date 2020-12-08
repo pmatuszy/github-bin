5828180945d389ef09ea0315c3b46c23c7fbb08c
root <root@backupche>
2020-12-08 10:16:40 +0100

    "new push from backupche @ 2020.12.08 10:16:40"

--- check-raspberry-hw-version.sh
+++ check-raspberry-hw-version.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# 2020.12.08 - v. 0.2 - adding info about total RAM
 # 2020.0x.xx - v. 0.1 - initial release (date unknown)
 
-echo `cat /proc/device-tree/model` ",   " `cat /proc/meminfo |grep MemTotal|awk '{printf ("Total RAM: %.0f GB", $2/1024/1024)}'`
+# tr -d '\0'` below is to get rid of the message "warning: command substitution: ignored null byte in input"
+
+echo
+echo `cat /proc/device-tree/model|tr -d '\0'` ",   " `cat /proc/meminfo |grep MemTotal|awk '{printf ("Total RAM: %.0f GB", $2/1024/1024)}'`
 echo
 
