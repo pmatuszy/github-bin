ea43b20fa0234504adc66023c378af6c16dd8126
root <root@backupss.eth.r.matuszyk.com>
2021-01-06 08:57:43 +0100

    "new push from backupss @ 2021.01.06 08:57:43"

--- zamontuj-encrypted-backupss.sh
+++ zamontuj-encrypted-backupss.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# 2021.01.06 - v. 0.2 - added additional bind mountpoints
 # 2020.0x.xx - v. 0.1 - initial release (date unknown)
 
 zpool export zfs_encrypted_file 2>/dev/null
@@ -12,6 +13,11 @@ df -h /encrypted
 
 
 cryptsetup luksOpen /dev/vg_crypto/lv_do_luksa luks-on-lv
-mount -o noatime /dev/mapper/luks-on-lv /mnt/luks-raid1-enclosure-b
+mount -o noatime /dev/mapper/luks-on-lv_encA /mnt/luks-raid1-encA
+mount -o noatime /dev/mapper/luks-on-lv_encB /mnt/luks-raid1-encB
 
-mount -o bind,noatime /mnt/luks-raid1-enclosure-b/replication/rclone-user /rclone-jail/storage-master/replication
+mount -o bind,noatime /mnt/luks-raid1-encA/replication/rclone-user/_rclone /rclone-jail/storage-master/replicationA
+mount -o bind,noatime /mnt/luks-raid1-encB/replication/rclone-user/_rclone /rclone-jail/storage-master/replicationB
+mount -o bind,noatime /mnt/luks-raid1-encA/backup/rclone-user              /rclone-jail/storage-master/backupA
+mount -o bind,noatime /mnt/luks-raid1-encB/backup/rclone-user              /rclone-jail/storage-master/backupB
+
