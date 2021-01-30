f04b9f36890147586169cddf70e46df9f9586526
root <root@yt2podcast.eth.che.matuszyk.com>
2021-01-30 23:01:02 +0100

    "new push from yt2podcast.eth.che.matuszyk.com @ 2021.01.30 23:01:02"

---
+++ zamontuj-encrypted-yt2podcast.sh
@@ -0,0 +1,13 @@
+# 2021.01.30 - v. 0.1 - initial release (date unknown)
+
+df -h /encrypted
+
+nazwa_pliku=/encrypted.luks2
+
+cryptsetup luksOpen ${nazwa_pliku} encrypted_luks_file_in_root
+
+-- mkfs.ext4 /dev/mapper/encrypted_luks_file_in_root
+mount -o noatime /dev/mapper/encrypted_luks_file_in_root /encrypted
+
+df -h /encrypted
+echo
