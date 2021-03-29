e147bf6603cb030e3201c88d2f13e8e0d3f3d961
root <pmatuszy.login+github@matuszyk.com>
2021-03-29 08:24:20 +0200

    "new push from rpi3.eth.r.matuszyk.com @ 2021.03.29 08:24:20"

--- zamontuj-encrypted.sh
+++ zamontuj-encrypted.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
-# 2020.0x.xx - v. 0.1 - initial release (date unknown)
-
-zpool export zfs_encrypted_file 2>/dev/null
-zpool import -d /encrypted.zfs -l -a
-
-zpool status -v
-
-zfs mount zfs_encrypted_file/encrypted
+# 2021.01.30 - v. 0.1 - initial release (date unknown)
 
 df -h /encrypted
+
+nazwa_pliku=/encrypted.luks2
+
+cryptsetup luksOpen ${nazwa_pliku} encrypted_luks_file_in_root
+
+mount -o noatime /dev/mapper/encrypted_luks_file_in_root /encrypted
+
+df -h /encrypted
+echo
