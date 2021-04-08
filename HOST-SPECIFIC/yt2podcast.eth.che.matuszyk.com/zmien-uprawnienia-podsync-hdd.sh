ed6ed15dd6a60ad7b810cac529bf3e94daaed49e
root <pmatuszy.login+github@matuszyk.com>
2021-04-08 23:12:05 +0200

    "new push from yt2podcast.eth.che.matuszyk.com @ 2021.04.08 23:12:05"

---
+++ HOST-SPECIFIC/yt2podcast.eth.che.matuszyk.com/zmien-uprawnienia-podsync-hdd.sh
@@ -0,0 +1,14 @@
+# 2021.02.07 - v. 0.2 - spr. czy nie ma wiecej dzialajacych instancji skryptu....
+# 2020.xx.xx - v. 0.1 - initial release
+
+# w dniu 21.01.2021 dodalem 2>/dev/null na koncu kazdego find'a by nie dostawac co jakis czas maili, ze plik nie istnieje z cronta
+
+# spr. ile jest dzialajacych instancji - jesli wiecej niz jedna to my wychodzimy
+if [[ `ps -e|grep $0` -gt 1 ]]; then
+  exit 2
+fi
+
+find /podsync-hdd/ -type f ! -group www-data -regex '.*\.\(mp3\|mp4\|webm\|xml\|m4a\)' -exec chgrp -v www-data {} \;  2>/dev/null
+find /podsync-hdd/ -type f ! -perm 640       -regex '.*\.\(mp3\|mp4\|webm\|xml\|m4a\)' -exec chmod -v 640 {} \;       2>/dev/null
+find /podsync-hdd/ -type d ! -perm 750       -exec chmod 750 -v {} \;                                                 2>/dev/null
+find /podsync-hdd/ -type d ! -group www-data -exec chgrp www-data -v {} \;                                            2>/dev/null
