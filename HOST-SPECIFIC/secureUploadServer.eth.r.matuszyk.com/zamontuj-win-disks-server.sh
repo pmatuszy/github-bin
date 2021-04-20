aa3d142396a7687f29754e19dda70bdd23cdb3b6
root <pmatuszy.login+github@matuszyk.com>
2021-04-20 10:06:33 +0200

    "new push from secureuploadserver.eth.r.matuszyk.com @ 2021.04.20 10:06:33"

--- HOST-SPECIFIC/secureUploadServer.eth.r.matuszyk.com/zamontuj-win-disks-server.sh
+++ HOST-SPECIFIC/secureUploadServer.eth.r.matuszyk.com/zamontuj-win-disks-server.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# 2021.04.20 - v. 0.1 - zmiana, by bylo jedno tylko pytanie o haslo
+# 2020.xx.xx - v. 0.1 - initial release
+
 . /root/_script_header.sh
 
 set +x 
@@ -8,13 +12,17 @@ echo  server
 echo 
 echo 
 
-set -x
+#set -x
 
-mount.cifs -o user=administrator //server.int.matuszyk.com/DyskC /mnt/server/DyskC ; df -hP /mnt/server/DyskC
-mount.cifs -o user=administrator //server.int.matuszyk.com/DyskD /mnt/server/DyskD ; df -hP /mnt/server/DyskD
-mount.cifs -o user=administrator //server.int.matuszyk.com/DyskN /mnt/server/DyskN ; df -hP /mnt/server/DyskN
-mount.cifs -o user=administrator //server.int.matuszyk.com/DyskO /mnt/server/DyskO ; df -hP /mnt/server/DyskO
+read -p "Wpisz haslo: " -s PASSWD
 
-set +x
+umount /mnt/server/Dysk?  2>/dev/null  # just in case sa zamontowane, by nie dostawac komunikatu "mount error(16): Device or resource busy"
+
+mount.cifs -o user=administrator,password=$PASSWD //server.int.matuszyk.com/DyskC /mnt/server/DyskC ; df -hP /mnt/server/DyskC
+mount.cifs -o user=administrator,password=$PASSWD  //server.int.matuszyk.com/DyskD /mnt/server/DyskD ; df -hP /mnt/server/DyskD
+mount.cifs -o user=administrator,password=$PASSWD //server.int.matuszyk.com/DyskN /mnt/server/DyskN ; df -hP /mnt/server/DyskN
+mount.cifs -o user=administrator,password=$PASSWD //server.int.matuszyk.com/DyskO /mnt/server/DyskO ; df -hP /mnt/server/DyskO
+
+# set +x
 
 . /root/_script_footer.sh
--- HOST-SPECIFIC/secureUploadServer.eth.r.matuszyk.com/zamontuj-win-disks-superintel.sh
+++ HOST-SPECIFIC/secureUploadServer.eth.r.matuszyk.com/zamontuj-win-disks-superintel.sh
@@ -1,4 +1,8 @@
-. /root/_script_header.sh
+#!/bin/bash
+# 2021.04.20 - v. 0.1 - zmiana, by bylo jedno tylko pytanie o haslo
+# 2020.xx.xx - v. 0.1 - initial release
+
+. /root/bin/_script_header.sh
 
 # mount -o remount /mnt/superintel/DyskC
 # mount -o remount /mnt/superintel/DyskD
@@ -13,16 +17,18 @@ echo superintel
 echo 
 echo 
 
-set -x
+read -p "Wpisz haslo: " -s PASSWD
+echo 
 
-mount.cifs -o user=administrator //superintel.eth.b.matuszyk.com/DyskC /mnt/superintel/DyskC ; df -hP /mnt/superintel/DyskC
-mount.cifs -o user=administrator //superintel.eth.b.matuszyk.com/DyskD /mnt/superintel/DyskD ; df -hP /mnt/superintel/DyskD
-mount.cifs -o user=administrator //superintel.eth.b.matuszyk.com/DyskE /mnt/superintel/DyskE ; df -hP /mnt/superintel/DyskE
-mount.cifs -o user=administrator //superintel.eth.b.matuszyk.com/DyskI /mnt/superintel/DyskI ; df -hP /mnt/superintel/DyskI
-mount.cifs -o user=administrator //superintel.eth.b.matuszyk.com/DyskS /mnt/superintel/DyskS ; df -hP /mnt/superintel/DyskS
-mount.cifs -o user=administrator //superintel.eth.b.matuszyk.com/DyskT /mnt/superintel/DyskT ; df -hP /mnt/superintel/DyskT
-mount.cifs -o user=administrator //superintel.eth.b.matuszyk.com/DyskU /mnt/superintel/DyskU ; df -hP /mnt/superintel/DyskU
+umount /mnt/superintel/Dysk?  2>/dev/null  # just in case sa zamontowane, by nie dostawac komunikatu "mount error(16): Device or resource busy"
+sleep 1
 
-set +x 
+mount.cifs -o user=administrator,password=$PASSWD //superintel.eth.b.matuszyk.com/DyskC /mnt/superintel/DyskC ; df -hP /mnt/superintel/DyskC
+mount.cifs -o user=administrator,password=$PASSWD //superintel.eth.b.matuszyk.com/DyskD /mnt/superintel/DyskD ; df -hP /mnt/superintel/DyskD
+mount.cifs -o user=administrator,password=$PASSWD //superintel.eth.b.matuszyk.com/DyskE /mnt/superintel/DyskE ; df -hP /mnt/superintel/DyskE
+mount.cifs -o user=administrator,password=$PASSWD //superintel.eth.b.matuszyk.com/DyskI /mnt/superintel/DyskI ; df -hP /mnt/superintel/DyskI
+mount.cifs -o user=administrator,password=$PASSWD //superintel.eth.b.matuszyk.com/DyskS /mnt/superintel/DyskS ; df -hP /mnt/superintel/DyskS
+mount.cifs -o user=administrator,password=$PASSWD //superintel.eth.b.matuszyk.com/DyskT /mnt/superintel/DyskT ; df -hP /mnt/superintel/DyskT
+mount.cifs -o user=administrator,password=$PASSWD //superintel.eth.b.matuszyk.com/DyskU /mnt/superintel/DyskU ; df -hP /mnt/superintel/DyskU
 
-. /root/_script_footer.sh
+. /root/bin/_script_footer.sh
