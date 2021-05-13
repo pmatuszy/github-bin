f918d22a105146a86c6b414d81a34a3a674d2bcd
root <pmatuszy.login+github@matuszyk.com>
2021-05-13 15:29:55 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2021.05.13 15:29:55"

--- cpu-temp.sh
+++ cpu-temp.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # 2020.0x.xx - v. 0.1 - initial release (date unknown)
 
-. /root/_script_header.sh
+. /root/bin/_script_header.sh
 
 while : ; do
   echo `date ` `awk '{printf "%3.1f C\n", $1/1000}' /sys/class/thermal/thermal_zone0/temp`
   sleep 3
 done
 
-. /root/_script_footer.sh
-
+. /root/bin/_script_footer.sh
