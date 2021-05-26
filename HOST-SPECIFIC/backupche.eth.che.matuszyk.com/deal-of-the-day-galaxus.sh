794e958862e942fb203e7d21244366198ce78d42
root <pmatuszy.login+github@matuszyk.com>
2021-05-27 00:31:51 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2021.05.27 00:31:51"

--- HOST-SPECIFIC/backupche.eth.che.matuszyk.com/deal-of-the-day-galaxus.sh
+++ HOST-SPECIFIC/backupche.eth.che.matuszyk.com/deal-of-the-day-galaxus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 2021.05.26 - v. 0.2 - added XDG_RUNTIME_DIR,XDG_DATA_DIR
+# 2021.05.26 - v. 0.2 - added XDG_RUNTIME_DIR,XDG_DATA_DIR, added -u option
 # 2021.05.20 - v. 0.1 - initial release (date unknown)
 
 export XDG_DATA_DIR=/encrypted/root/XDG_DATA_HOME
@@ -16,7 +16,7 @@ echo "https://www.galaxus.ch/en/LiveShopping" > "${zawartosc_maila}"
 
 mpack -s "(`date '+%Y.%m.%d %H:%M'`) galaxus.ch-Deal of the Day" -c image/jpeg "${plik_po_cropie}" -d "${zawartosc_maila}" matuszyk@matuszyk.com
 
-/opt/signal-cli/bin/signal-cli -U +41763691467 send -m "(`date '+%Y.%m.%d %H:%M'`) Galaxus.ch-Deal of the Day, https://www.galaxus.ch/en/LiveShopping" -a "${plik_po_cropie}" --note-to-self 2>/dev/null
+/opt/signal-cli/bin/signal-cli -u +41763691467 send -m "(`date '+%Y.%m.%d %H:%M'`) Galaxus.ch-Deal of the Day, https://www.galaxus.ch/en/LiveShopping" -a "${plik_po_cropie}" --note-to-self 2>/dev/null
 
 rm "${plik_po_cropie}" "${plik_bez_cropa}"
 
