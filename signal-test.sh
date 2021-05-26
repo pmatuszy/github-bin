dbfc4808f0e8ac67eada2bfd10f7c5adf60d7fb2
root <pmatuszy.login+github@matuszyk.com>
2021-05-26 23:51:39 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2021.05.26 23:51:39"

--- signal-test.sh
+++ signal-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# 2021.05.26 - v. 0.2 - added XDG_RUNTIME_DIR
 # 2021.05.20 - v. 0.1 - initial release
 
 
@@ -9,5 +9,7 @@ if [ ! -f /opt/signal-cli/bin/signal-cli ];then
   echo 
   exit 1
 fi
+
+XDG_RUNTIME_DIR=/encrypted/root/XDG_DATA_HOME
 
 time /opt/signal-cli/bin/signal-cli send -m "[`date '+%Y.%m.%d %H:%M:%S'`] test from `hostname`" --note-to-self
