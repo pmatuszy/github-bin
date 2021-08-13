ef92c3c0b563349141b9078b0282ed9661c8e905
root <pmatuszy.login+github@matuszyk.com>
2021-08-13 08:49:44 +0200

    "new push from yt2podcast.eth.che.matuszyk.com @ 2021.08.13 08:49:44"

--- sprawdz-czy-dziala-strona-wyslij-maila-jak-NIE-dziala.sh
+++ sprawdz-czy-dziala-strona-wyslij-maila-jak-NIE-dziala.sh
@@ -1,3 +1,3 @@
 if [ `wget yt2podcast.com:8080 -qO - |grep .xml|wc -l` -eq 0 ];then 
-   echo "!!! NIE dziala strona yt2podcast.com:8080" | strings | /usr/bin/mailx -s " [ `hostname` ] PROBLEM - strona yt2podcast.com:8080 NIE dziala" matuszyk+`hostname`@matuszyk.com
+   echo "!!! NIE dziala strona yt2podcast.com:8080" | strings | /usr/bin/mailx -s " [ `hostname --short` ] PROBLEM - strona yt2podcast.com:8080 NIE dziala" matuszyk+`hostname`@matuszyk.com
 fi
