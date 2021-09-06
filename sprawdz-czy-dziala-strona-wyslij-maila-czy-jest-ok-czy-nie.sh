9b679d8611999672f28f5640c30b67622acb0905
root <pmatuszy.login+github@matuszyk.com>
2021-09-06 10:00:25 +0200

    "new push from yt2podcast.eth.che.matuszyk.com @ 2021.09.06 10:00:25"

--- sprawdz-czy-dziala-strona-wyslij-maila-czy-jest-ok-czy-nie.sh
+++ sprawdz-czy-dziala-strona-wyslij-maila-czy-jest-ok-czy-nie.sh
@@ -1,5 +1,5 @@
 if [ `wget yt2podcast.com:8080 -qO - |grep .xml|wc -l` -gt 0 ];then 
-   echo dziala strona yt2podcast.com:8080 | strings | /usr/bin/mailx -s "(`hostname --short`) OK - strona yt2podcast.com:8080 dziala" matuszyk+`hostname`@matuszyk.com
+   echo dziala strona yt2podcast.com:8080 | strings | aha | /usr/bin/mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`hostname --short`) OK - strona yt2podcast.com:8080 dziala" matuszyk+`hostname`@matuszyk.com
 else 
-   echo "!!! NIE dziala strona yt2podcast.com:8080" | strings | /usr/bin/mailx -s "(`hostname --short`) PROBLEM - strona yt2podcast.com:8080 NIE dziala" matuszyk+`hostname`@matuszyk.com
+   echo "!!! NIE dziala strona yt2podcast.com:8080" | strings | aha | /usr/bin/mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`hostname --short`) PROBLEM - strona yt2podcast.com:8080 NIE dziala" matuszyk+`hostname`@matuszyk.com
 fi
