972bf5559632373d3aca6e427009123629c4a076
root <pmatuszy.login+github@matuszyk.com>
2022-01-05 12:14:48 +0100

    "new push from backupche.eth.che.matuszyk.com @ 2022.01.05 12:14:48"

--- HOST-SPECIFIC/backupche.eth.che.matuszyk.com/sprawdz-aktualizacje-firmware-upgrade-check-gopro4.sh
+++ HOST-SPECIFIC/backupche.eth.che.matuszyk.com/sprawdz-aktualizacje-firmware-upgrade-check-gopro4.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# 2022.01.05 - v. 0.6 - zmiana cutycapt na firefoxa bo nie generowala sie strona ladnie
 # 2021.11.04 - v. 0.5 - dodanie *crop_offset i temat_maila
 # 2021.08.06 - v. 0.4 - sending dbus signal message
 # 2021.06.22 - v. 0.3 - URL set and used throughout the script, added timeout as signal-cli can be run in the background and this script would never finish
@@ -11,24 +12,25 @@ export XDG_RUNTIME_DIR=/run/user/0
 export URL="https://gopro.com/en/ch/update/hero4"
 temat_maila="(`date '+%Y.%m.%d %H:%M'`) GoPro4 firmware update"
 
-timeout=300
-kill_after=310
-rozmiar_x_ekran=900
-rozmiar_y_ekran=700
-rozmiar_x_crop=800
-rozmiar_y_crop=700
-rozmiar_x_crop_offset=260
-rozmiar_y_crop_offset=90
+timeout=80
+kill_after=85
+rozmiar_x_ekran=1100
+rozmiar_y_ekran=900
+rozmiar_x_crop=710
+rozmiar_y_crop=650
+rozmiar_x_crop_offset=330
+rozmiar_y_crop_offset=180
 
 max_wait_na_strone=30000       # w ms
 delay_po_wczytaniu_strony=1000 # w ms
 
-javascript=off
-
-plik_bez_cropa=`mktemp --dry-run --suffix=-bez-cropa.jpg`
+plik_bez_cropa=`TMPDIR=$(pwd) mktemp --dry-run --suffix=-bez-cropa.jpg`
 plik_po_cropie=`mktemp --dry-run --suffix=-po-cropie.jpg`
 zawartosc_maila=`mktemp --dry-run --suffix=.txt`
-xvfb-run --server-args="-screen 0, ${rozmiar_x_ekran}x${rozmiar_y_ekran}x24" cutycapt --max-wait=${max_wait_na_strone} --delay=${delay_po_wczytaniu_strony} --min-width=${rozmiar_x_ekran} --min-height=${rozmiar_y_ekran} --javascript=${javascript} --url=${URL} --out="${plik_bez_cropa}"
+
+export DISPLAY=
+
+/usr/bin/timeout --preserve-status --kill-after=$kill_after $timeout chromium --headless --no-sandbox --disable-gpu --ignore-certificate-error --ignore-ssl-errors --hide-scrollbars --window-size="${rozmiar_x_ekran},${rozmiar_y_ekran}" --screenshot="${plik_bez_cropa}" "${URL}" 2>/dev/null
 
 convert "${plik_bez_cropa}" -crop ${rozmiar_x_crop}x${rozmiar_y_crop}+${rozmiar_x_crop_offset}+${rozmiar_y_crop_offset} "${plik_po_cropie}"
 
@@ -37,6 +39,6 @@ echo "${URL}" > "${zawartosc_maila}"
 mpack -s "${temat_maila}" -c image/jpeg "${plik_po_cropie}" -d "${zawartosc_maila}" matuszyk@matuszyk.com
 
 # /usr/bin/timeout --preserve-status --kill-after=$kill_after $timeout /opt/signal-cli/bin/signal-cli -u +41763691467 send -m "(`date '+%Y.%m.%d %H:%M'`) digitec.ch-Deal of the Day, ${URL}" -a "${plik_po_cropie}" --note-to-self 2>&1 > /dev/null
-/usr/bin/timeout --preserve-status --kill-after=$kill_after $timeout /usr/bin/dbus-send --session --type=method_call --print-reply --dest="org.asamk.Signal" /org/asamk/Signal org.asamk.Signal.sendMessage string:"[`date '+%Y.%m.%d %H:%M:%S'`]" array:string:"${plik_po_cropie}" string:+41763691467
+/usr/bin/timeout --preserve-status --kill-after=$kill_after $timeout /usr/bin/dbus-send --session --type=method_call --print-reply --dest="org.asamk.Signal" /org/asamk/Signal org.asamk.Signal.sendMessage string:"[`date '+%Y.%m.%d %H:%M:%S'`] ${URL}" array:string:"${plik_po_cropie}" string:+41763691467
 
-rm "${plik_po_cropie}" "${plik_bez_cropa}"
+rm "${plik_po_cropie}" "${plik_bez_cropa}" "${zawartosc_maila}"
