965f1c2ce165670494599ab4e3617113793fa5f0
root <pmatuszy.login+github@matuszyk.com>
2022-02-20 18:11:31 +0100

    "new push from backupche.eth.che.matuszyk.com @ 2022.02.20 18:11:31"

--- HOST-SPECIFIC/backupche.eth.che.matuszyk.com/zamontuj-win-disks-desktopche.sh
+++ HOST-SPECIFIC/backupche.eth.che.matuszyk.com/zamontuj-win-disks-desktopche.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
+# 2022.02.20 - v. 0.3 - zmiana nazwy komputera i dodano mkdir -p
 # 2021.04.09 - v. 0.2 - changed IP to machine DNS name 
 # 2020.0x.xx - v. 0.1 - initial release (date unknown)
 
 
-. /root/_script_header.sh
+. /root/bin/_script_header.sh
 
 set +x
 
 echo
 echo
-echo  desktopche
+echo  pgm-che
 echo
 echo
+
+mkdir -p /mnt/pgm-che/DyskC /mnt/pgm-che/DyskD /mnt/pgm-che/DyskE /mnt/pgm-che/DyskF
 
 set -x
 
-mount.cifs -o user=administrator //pgm-desktop-che.eth.che.matuszyk.com/DyskC /mnt/desktopche/DyskC ; df -hP /mnt/desktopche/DyskC
-mount.cifs -o user=administrator //pgm-desktop-che.eth.che.matuszyk.com/DyskD /mnt/desktopche/DyskD ; df -hP /mnt/desktopche/DyskD
-mount.cifs -o user=administrator //pgm-desktop-che.eth.che.matuszyk.com/DyskE /mnt/desktopche/DyskE ; df -hP /mnt/desktopche/DyskE
+mount.cifs -o user=administrator //pgm-che.eth.che.matuszyk.com/DyskC /mnt/pgm-che/DyskC ; df -hP /mnt/desktopche/DyskC
+mount.cifs -o user=administrator //pgm-che.eth.che.matuszyk.com/DyskD /mnt/pgm-che/DyskD ; df -hP /mnt/desktopche/DyskD
+mount.cifs -o user=administrator //pgm-che.eth.che.matuszyk.com/DyskE /mnt/pgm-che/DyskE ; df -hP /mnt/desktopche/DyskE
 
 set +x
 
-. /root/_script_footer.sh
+. /root/bin/_script_footer.sh
 
