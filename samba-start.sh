9e7a591f8a46746fb53e474b116c30b59a9e7401
root <pmatuszy.login+github@matuszyk.com>
2022-03-03 15:08:35 +0100

    "new push from lublin.eth.b.matuszyk.com @ 2022.03.03 15:08:35"

---
+++ samba-start.sh
@@ -0,0 +1,5 @@
+# 2020.12.25 - v. 0.1 - initial release
+
+set -x
+systemctl start smbd.service nmbd.service
+set +x
---
+++ samba-stop.sh
@@ -0,0 +1,5 @@
+# 2020.12.25 - v. 0.1 - initial release
+
+set -x
+systemctl stop smbd.service nmbd.service
+set +x
