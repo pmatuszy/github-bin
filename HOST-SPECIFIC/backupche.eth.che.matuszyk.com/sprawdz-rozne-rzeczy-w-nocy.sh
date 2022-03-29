5ec93ab397068faae903ec5553d9b7bdb7f1ed33
root <pmatuszy.login+github@matuszyk.com>
2022-03-29 10:11:56 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2022.03.29 10:11:56"

--- HOST-SPECIFIC/backupche.eth.che.matuszyk.com/sprawdz-rozne-rzeczy-w-nocy.sh
+++ HOST-SPECIFIC/backupche.eth.che.matuszyk.com/sprawdz-rozne-rzeczy-w-nocy.sh
@@ -6,12 +6,12 @@ odstep_miedzy_wywolaniami=4m
 for p in /root/bin/deal-of-the-day-digitec.sh \
          /root/bin/deal-of-the-day-galaxus.sh \
          /root/bin/WD-GOLD-16TB-digitec.sh \
-         /root/bin/sprawdz-aktualizacje-firmware-upgrade-check-gopro4.sh \
-         /root/bin/sprawdz-aktualizacje-firmware-upgrade-check-gopro7.sh \
-         /root/bin/sprawdz-aktualizacje-firmware-upgrade-check-gopro10.sh \
-         /root/bin/sprawdz-aktualizacje-firmware-upgrade-check-GPSMAP-66.sh \
-         /root/bin/sprawdz-aktualizacje-firmware-upgrade-check-nuci7.sh \
-         /root/bin/sprawdz-aktualizacje-firmware-upgrade-check-nucvm.sh \
+         /root/bin/spr-gopro4.sh \
+         /root/bin/spr-gopro7.sh \
+         /root/bin/spr-gopro10.sh \
+         /root/bin/spr-GPSMAP-66.sh \
+         /root/bin/spr-nuci7.sh \
+         /root/bin/spr-nucvm.sh \
          /root/bin/sprawdz-aktualizacje-veracrypt.sh 
   do
 
