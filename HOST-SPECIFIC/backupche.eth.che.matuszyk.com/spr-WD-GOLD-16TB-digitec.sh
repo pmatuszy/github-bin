577a93963671feb72c64177fafae9359c5f9ce6a
root <pmatuszy.login+github@matuszyk.com>
2022-03-29 12:26:23 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2022.03.29 12:26:23"

--- HOST-SPECIFIC/backupche.eth.che.matuszyk.com/spr-WD-GOLD-16TB-digitec.sh
+++ HOST-SPECIFIC/backupche.eth.che.matuszyk.com/spr-WD-GOLD-16TB-digitec.sh
@@ -9,21 +9,34 @@ export XDG_DATA_DIR=/encrypted/root/XDG_DATA_HOME
 export XDG_RUNTIME_DIR=/run/user/0
 
 export URL="https://www.digitec.ch/en/s1/product/wd-gold-16tb-35-hard-drives-13424026"
+temat_maila="(`date '+%Y.%m.%d %H:%M'`) digitec.ch-Deal of the Day"
+
 timeout=300
 kill_after=310
+rozmiar_x_ekran=900
+rozmiar_y_ekran=900
+rozmiar_x_crop=800
+rozmiar_y_crop=800
+rozmiar_x_crop_offset=3
+rozmiar_y_crop_offset=120
+max_wait_na_strone=30000       # w ms
+delay_po_wczytaniu_strony=1000 # w ms
+
+javascript=off
 
 plik_bez_cropa=`mktemp --dry-run --suffix=-bez-cropa.jpg`
 plik_po_cropie=`mktemp --dry-run --suffix=-po-cropie.jpg`
 zawartosc_maila=`mktemp --dry-run --suffix=.txt`
-xvfb-run --server-args="-screen 0, 800x600x24" cutycapt --max-wait=30000 --delay=5000 --url=${URL} --out="${plik_bez_cropa}"
+xvfb-run --server-args="-screen 0, ${rozmiar_x_ekran}x${rozmiar_y_ekran}x24" cutycapt --max-wait=${max_wait_na_strone} --delay=${delay_po_wczytaniu_strony} --min-width=${rozmiar_x_ekran} --min-height=${rozmiar_y_ekran} --javascript=${javascript} --url=${URL} --out="${plik_bez_cropa}"
 
-convert "${plik_bez_cropa}" -crop 800x800+3+5 "${plik_po_cropie}"
+convert "${plik_bez_cropa}" -crop ${rozmiar_x_crop}x${rozmiar_y_crop}+${rozmiar_x_crop_offset}+${rozmiar_y_crop_offset} "${plik_po_cropie}"
 
 echo "${URL}" > "${zawartosc_maila}"
 
-mpack -s "(`date '+%Y.%m.%d %H:%M'`)digitec.ch-WD GOLD 16TB" -c image/jpeg "${plik_po_cropie}" -d "${zawartosc_maila}" matuszyk@matuszyk.com
+mpack -s "${temat_maila}" -c image/jpeg "${plik_po_cropie}" -d "${zawartosc_maila}" matuszyk@matuszyk.com
 
-# /usr/bin/timeout --preserve-status --kill-after=$kill_after $timeout /opt/signal-cli/bin/signal-cli -u +41763691467 send -m "(`date '+%Y.%m.%d %H:%M'`) digitec.ch-Deal of the Day, ${URL}" -a "${plik_po_cropie}" --note-to-self >/dev/null
+# /usr/bin/timeout --preserve-status --kill-after=$kill_after $timeout /opt/signal-cli/bin/signal-cli -u +41763691467 send -m "(`date '+%Y.%m.%d %H:%M'`) digitec.ch-Deal of the Day, ${URL}" -a "${plik_po_cropie}" --note-to-self 2>&1 > /dev/null
 /usr/bin/timeout --preserve-status --kill-after=$kill_after $timeout /usr/bin/dbus-send --session --type=method_call --print-reply --dest="org.asamk.Signal" /org/asamk/Signal org.asamk.Signal.sendMessage string:"[`date '+%Y.%m.%d %H:%M:%S'`] ${URL}" array:string:"${plik_po_cropie}" string:+41763691467
 
 rm "${plik_po_cropie}" "${plik_bez_cropa}"
+
