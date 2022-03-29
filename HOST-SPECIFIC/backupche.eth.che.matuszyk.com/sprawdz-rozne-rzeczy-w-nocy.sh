157b56689da4f7211c5f67c58c2890cab7970ebc
root <pmatuszy.login+github@matuszyk.com>
2022-03-29 12:08:14 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2022.03.29 12:08:14"

--- HOST-SPECIFIC/backupche.eth.che.matuszyk.com/sprawdz-rozne-rzeczy-w-nocy.sh
+++ HOST-SPECIFIC/backupche.eth.che.matuszyk.com/sprawdz-rozne-rzeczy-w-nocy.sh
@@ -2,7 +2,7 @@
 # 2022.03.29 - v. 0.2 - zmiana na krotsze nazwy skrypow bo screen sobie z dlugimi nie radzi, skrocony czas miedzy wywolaniami screena z 4m do 10s
 # 2022.03.05 - v. 0.1 - initial release (date unknown)
 
-odstep_miedzy_wywolaniami=10s
+odstep_miedzy_wywolaniami=30s
 
 for p in /root/bin/deal-of-the-day-digitec.sh \
          /root/bin/deal-of-the-day-galaxus.sh \
