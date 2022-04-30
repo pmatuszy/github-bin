94c57d19a0f822348297e37e34a7d13f58958c55
root <pmatuszy.login+github@matuszyk.com>
2022-04-30 17:18:50 +0200

    "new push from rpi42b.eth.b.matuszyk.com @ 2022.04.30 17:18:50"

--- ssd-trim.sh
+++ ssd-trim.sh
@@ -3,6 +3,8 @@
 # 2022.04.30 - v. 0.2 - added healthcheck support
 # 2021.02.24 - v. 0.1 - initial release
 
+. /root/bin/_script_header.sh
+
 if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep '^ssd-trim'|awk '{print $2}')
 fi
@@ -15,3 +17,6 @@ m=$( /sbin/fstrim  --verbose --all 2>&1)
 /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
 
 echo
+
+. /root/bin/_script_footer.sh
+
