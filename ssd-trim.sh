2d226bbff7baf8848715f0fc82185abb8efde119
root <pmatuszy.login+github@matuszyk.com>
2022-05-03 23:14:16 +0200

    "new push from backupss.eth.ss.matuszyk.com @ 2022.05.03 23:14:16"

--- ssd-trim.sh
+++ ssd-trim.sh
@@ -19,3 +19,12 @@ fi
 
 echo
 . /root/bin/_script_footer.sh
+
+exit
+
+0 */4 * * *     /root/bin/ssd-trim.sh 2>&1 > /dev/null
+#0 6-22/4 * * *     /root/bin/ssd-trim.sh 2>&1 > /dev/null
+#0 2      * * 0,2-6 /root/bin/ssd-trim.sh 2>&1 > /dev/null
+#0 2      * * 1     /root/bin/ssd-trim.sh | strings | aha | /usr/bin/mailx -r root@`hostname` -a 'Content-Type: text/html' -s "ssd-trim.sh@`/bin/hostname`-`date '+\%Y.\%m.\%d \%H:\%M:\%S'`" matuszyk+`/bin/hostname`@matuszyk.com
+~
+
