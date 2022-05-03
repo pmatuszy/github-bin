907240e261a94708dffd5b3f5bcd6c09831a4f30
root <pmatuszy.login+github@matuszyk.com>
2022-05-03 22:34:27 +0200

    "new push from zurich.eth.che.matuszyk.com @ 2022.05.03 22:34:27"

--- sprawdz-czy-dziala-server-vpn.sh
+++ sprawdz-czy-dziala-server-vpn.sh
@@ -8,7 +8,7 @@ fi
 
 # spr. czy dziala vpn
 if [ `ps -ef|grep vpnserver | awk '{print $8}'|grep -v grep|uniq|wc -l` -eq 0 ];then 
-  (echo "vpn na `hostname` NIE dziala" | mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`/bin/hostname`-`date '+\%Y.\%m.\%d \%H:\%M:\%S'`) vpn nie dziala" matuszyk+`/bin/hostname`@matuszyk.com)
+#  (echo "vpn na `hostname` NIE dziala" | mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`/bin/hostname`-`date '+\%Y.\%m.\%d \%H:\%M:\%S'`) vpn nie dziala" matuszyk+`/bin/hostname`@matuszyk.com)
   /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
 else
   /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
