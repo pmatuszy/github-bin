5e1c0d885f0d6ae46d0b1f4ef395ebcef8f0a682
root <pmatuszy.login+github@matuszyk.com>
2022-05-03 11:46:52 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2022.05.03 11:46:52"

--- sprawdz-czy-dziala-strona-yt2podcast.com.sh
+++ sprawdz-czy-dziala-strona-yt2podcast.com.sh
@@ -1,5 +1,19 @@
+
+
+. /root/bin/_script_header.sh
+
+if [ -f "$HEALTHCHECKS_FILE" ];then
+  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep '^ssd-trim'|awk '{print $2}')
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL"/start 2>/dev/null
+fi
+
+
 if [ `wget yt2podcast.com:8080 -qO - |grep .xml|wc -l` -gt 0 ];then 
    echo dziala strona yt2podcast.com:8080 | strings | aha | /usr/bin/mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`hostname --short`) OK - strona yt2podcast.com:8080 dziala" matuszyk+`hostname`@matuszyk.com
+   /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
 else 
    echo "!!! NIE dziala strona yt2podcast.com:8080" | strings | aha | /usr/bin/mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`hostname --short`) PROBLEM - strona yt2podcast.com:8080 NIE dziala" matuszyk+`hostname`@matuszyk.com
+   /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
 fi
+
+. /root/bin/_script_footer.sh
