8dc0571f3effb7ce22c914035d66b3bf10671108
root <pmatuszy.login+github@matuszyk.com>
2022-05-04 00:11:16 +0200

    "new push from rpi42b.eth.b.matuszyk.com @ 2022.05.04 00:11:16"

--- sprawdz-ile-apt-list--upgradable.sh
+++ sprawdz-ile-apt-list--upgradable.sh
@@ -6,20 +6,22 @@ if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
 fi
 
-ile_max_by_nie_raportowac=2
+ile_max_by_nie_raportowac=40
 
 /usr/bin/apt-get update 2>&1 >/dev/null
 
-ile_jest_do_upgradeowania=$(/usr/bin/apt-get list --upgradable 2>/dev/null|wc -l)
+ile_jest_do_upgradeowania=$(/usr/bin/apt list --upgradable 2>/dev/null|wc -l)
 
-if [ $ile_jest_do_upgradeowania -gt $ile_max_by_nie_raportowac ];then 
-  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
+m=$(echo ile_jest_do_upgradeowania = $ile_jest_do_upgradeowania ; echo ile_max_by_nie_raportowac = $ile_max_by_nie_raportowac ; /usr/bin/apt list --upgradable 2>&1)
+
+if [ $ile_jest_do_upgradeowania -gt $ile_max_by_nie_raportowac ];then
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
 else
-  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
 fi
 
 exit
 #####
 # new crontab entry
 
-2 6 * * * /root/bin/sprawdz-czy-dziala-server-vpn.sh
+2 */6 * * * /root/bin/sprawdz-ile-apt-list--upgradable.sh
