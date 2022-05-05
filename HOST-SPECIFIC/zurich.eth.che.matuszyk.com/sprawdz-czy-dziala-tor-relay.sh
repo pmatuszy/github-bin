f19c009cc84c2d799b71be323ac3115b5166c7fe
root <pmatuszy.login+github@matuszyk.com>
2022-05-05 16:56:45 +0200

    "new push from zurich.eth.che.matuszyk.com @ 2022.05.05 16:56:45"

---
+++ HOST-SPECIFIC/zurich.eth.che.matuszyk.com/sprawdz-czy-dziala-tor-relay.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# 2022.05.03 - v. 0.1 - initial release (date unknown)
+
+. /root/bin/_script_header.sh
+if [ -f "$HEALTHCHECKS_FILE" ];then
+  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
+
+# spr. czy dziala tor relay
+if [ $(/bin/systemctl status tor|grep "Active: adctive"|wc -l) -eq 0 ];then 
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
+else
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
+fi
+
+exit
+#####
+# new crontab entry
+
+3 * * * * /root/bin/sprawdz-czy-dziala-tor-relay.sh
+
