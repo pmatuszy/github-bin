f8203c82d988dd0a4836ce63536e9d27df58f7d5
root <pmatuszy.login+github@matuszyk.com>
2022-05-06 10:39:25 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2022.05.06 10:39:25"

---
+++ HOST-SPECIFIC/backupche.eth.che.matuszyk.com/restic-backup-BXL-env.sh
@@ -0,0 +1,36 @@
+export RESTIC_BACKUP_NAME=restic_backup_BXL
+
+export WHAT_TO_BACKUP_ON_TOP_OF_ROOT="/mnt/luks-raid1-16tb_another/samba/OneNote-p /mnt/luks-raid1-16tb_another/samba/OneNote-shared /mnt/luks-raid1-16tb_another/samba/Mikey /mnt/luks-raid1-16tb_another/samba/OneNote-shared-Matusiaczki"
+
+export RESTIC_REPOSITORY=rclone:crypt-sftp-BXL-restic:/backupche.eth.che.matuszyk.com
+export RCLONE_CONFIG=/root/rclone.conf
+
+export WHAT_TO_BACKUP_ON_TOP_OF_ROOT="/mnt/luks-raid1-16tb_another/samba/OneNote-p /mnt/luks-raid1-16tb_another/samba/OneNote-shared /mnt/luks-raid1-16tb_another/samba/Mikey /mnt/luks-raid1-16tb_another/samba/OneNote-shared-Matusiaczki"
+
+MY_EXCLUDES='{/dev,/media,/mnt,/proc,/run,/sys,/tmp,/var/tmp,/veracrypt.vc,/encrypted.luks2,/rclone-jail,$XDG_CACHE_HOME,/root/.cache,/snap/**/*,/podsync-hdd}'
+MY_EXCLUDE_FILE=/root/restic_iexclude_file.txt
+
+RESTIC_BIN=/usr/bin/restic
+
+export RCLONE_CHECKERS=2
+export RCLONE_TRANSFERS=2
+export RCLONE_BUFFER_SIZE=128M
+export RCLONE_STATS_FILE_NAME_LENGTH=0
+export RCLONE_VERBOSE=0
+export RCLONE_STATS_LOG_LEVEL=INFO
+export RCLONE_STATS_ONE_LINE=true
+export RCLONE_STATS=1s
+
+# export RCLONE_BWLIMIT=30M
+
+# nie ograniczamy szybkosci bo uplink w CHE jest duzy
+# nie ograniczamy szybkosci bo uplink w CHE jest duzy
+# nie ograniczamy szybkosci bo uplink w CHE jest duzy
+# nie ograniczamy szybkosci bo uplink w CHE jest duzy
+# nie ograniczamy szybkosci bo uplink w CHE jest duzy
+
+# export RCLONE_BWLIMIT='Mon-01:00,5M Mon-07:00,3M Tue-01:00,5M Tue-07:00,3M Wed-01:00,5M Wed-07:00,3M Thu-01:00,5M Thu-07:00,3M Fri-01:00,5M Fri-07:00,3M Sat-01:00,5M Sat-07:00,3M Sun-01:00,5M Sun-08:00,3M'
+
+# export RCLONE_RC=1
+# export RCLONE_RC_ADDR=127.0.0.1:9558
+
---
+++ restic-backup-generic.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# 2022.05.06 - v. 1.3 - added check if we are run from CRON
+# 2022.05.04 - v. 1.2 - added healthcheck support, remove sensitive data from the script itself
+# 2021.04.16 - v. 1.1 - small change to exit message when XDG_CACHE_HOME is not defined
+# 2021.04.14 - v. 1.0 - added checks for RESTIC_BIN and XDG_CACHE_HOME, overhauld of the script
+# 2021.04.11 - v. 0.2 - added /bin/bash as the first line of the script
+# 2021.04.10 - v. 0.1 - initial release
+
+# exit when your script tries to use undeclared variables
+set -o nounset
+set -o pipefail
+
+. "${RESTIC_BACKUP_ENV_FILE}"
+
+. /root/bin/_script_header.sh
+
+if [ -f "$HEALTHCHECKS_FILE" ];then
+  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^${RESTIC_BACKUP_NAME}"|awk '{print $2}')
+fi
+
+REPO_PASS_INFO=/encrypted/root/scripts/repo-pass-info.sh
+if [ -f "$REPO_PASS_INFO" ]; then
+  . "$REPO_PASS_INFO"
+else
+  echo '#####################################################'
+  echo '#####################################################'
+  echo
+  echo "${REPO_PASS_INFO} nie moze byc znaleziony. Wychodze"
+  echo
+  echo '#####################################################'
+  echo '#####################################################'
+  exit 4
+fi
+
+# check if we are run from the cron
+CRON=$(pstree -s $$ | grep -q cron && echo true || echo false)
+
+if $CRON ; then
+  mail_subject=" ( `/bin/hostname`  - `date '+%Y.%m.%d %H:%M:%S'`) $(basename $0)"
+  mail_adressee=matuszyk+`/bin/hostname`@matuszyk.com
+  exec 2>&1 > >( strings | aha | /usr/bin/mailx -r root@`hostname` -a 'Content-Type: text/html' -s "$mail_subject" "$mail_adressee")
+fi
+
+if [ ! -f "$RESTIC_BIN" ]; then
+  echo '#####################################################'
+  echo '#####################################################'
+  echo
+  echo "restic binary defined as ${RESTIC_BIN} nie moze byc znaleziony. Wychodze"
+  echo
+  echo '#####################################################'
+  echo '#####################################################'
+  exit 2
+fi
+
+if [ -d "/root/restic-cache-dir" ]; then
+  export XDG_CACHE_HOME="/root/restic-cache-dir"
+fi
+
+if [ -d "/encrypted/root/restic-cache-dir" ]; then
+  export XDG_CACHE_HOME="/encrypted/root/restic-cache-dir"
+fi
+
+if [ ! -d "$XDG_CACHE_HOME" ] ; then
+   echo "XDG_CACHE_HOME ($XDG_CACHE_HOME)  nie istnieje"
+   echo "WYCHODZE ..."
+   exit 4
+fi
+
+if pgrep -f "${RESTIC_BIN}" > /dev/null ; then
+  echo '#####################################################'
+  echo '#####################################################'
+  echo
+  echo "${RESTIC_BIN} dziala, wiec nie startuje nowej instancji a po prostu koncze dzialanie skryptu"
+  echo
+  echo '#####################################################'
+  echo '#####################################################'
+  exit 2
+fi
+
+if [ ! -d "$XDG_CACHE_HOME" ] ; then
+   echo "$XDG_CACHE_HOME nie istnieje"
+   echo "WYCHODZE ..."
+   exit 1
+fi
+
+/usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL"/start 2>/dev/null
+echo ; echo "RESTIC_REPOSITORY = $RESTIC_REPOSITORY" ; echo
+
+backup_log=$(eval ${RESTIC_BIN} --cleanup-cache --iexclude=${MY_EXCLUDES} --iexclude-file=${MY_EXCLUDE_FILE} backup / $WHAT_TO_BACKUP_ON_TOP_OF_ROOT )
+kod_powrotu=$?
+
+m=$( echo "RESTIC_REPOSITORY = $RESTIC_REPOSITORY" ; echo ; echo "~~~~~~~~~~~~~~~~~~~~~~~~~"
+     echo kod powrotu z backupu: $kod_powrotu ; echo "~~~~~~~~~~~~~~~~~~~~~~~~~" ; echo ;
+     ${RESTIC_BIN} --cleanup-cache                          snapshots 2>&1 )
+
+# by output poszedl mailem...
+echo "$backup_log"
+echo "###########################################################"
+echo "$m"
+
+if (( $kod_powrotu != 0 )); then
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$backup_log" --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
+else
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$backup_log" --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
+fi
+
+. /root/bin/_script_footer.sh
