ac499fd23fbf0b5d73ec251aed4f7278c3fb5f44
root <pmatuszy.login+github@matuszyk.com>
2022-05-12 12:13:01 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2022.05.12 12:13:01"

---
+++ healthchecks-ping.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 2022.05.12 - v. 0.1 - initial release
+
+. /root/bin/_script_header.sh
+/usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
+. /root/bin/_script_footer.sh
+exit
+
+######
+template crontab entry:
+
+* * * * *   sleep $((RANDOM \% 60)) && /root/bin/healthchecks-ping.sh
