a8fa284fd97ed7a43bbc452a970612725f946491
root <pmatuszy.login+github@matuszyk.com>
2022-05-12 12:19:05 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2022.05.12 12:19:05"

--- healthchecks-ping.sh
+++ healthchecks-ping.sh
@@ -2,6 +2,11 @@
 # 2022.05.12 - v. 0.1 - initial release
 
 . /root/bin/_script_header.sh
+
+if [ -f "$HEALTHCHECKS_FILE" ];then
+  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
+
 /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
 . /root/bin/_script_footer.sh
 exit
