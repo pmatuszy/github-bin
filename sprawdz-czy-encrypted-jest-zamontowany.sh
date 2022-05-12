3105a348b02922d5bc8e4ae201d7c324de5d4eca
root <pmatuszy.login+github@matuszyk.com>
2022-05-12 08:07:07 +0200

    "new push from secureuploadserver.eth.r.matuszyk.com @ 2022.05.12 08:07:07"

--- sprawdz-czy-encrypted-jest-zamontowany.sh
+++ sprawdz-czy-encrypted-jest-zamontowany.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+# 2022.05.12 - v. 0.1 - usunieta zmienna m, ktora nie byla uzywana
 # 2022.05.03 - v. 0.1 - initial release (date unknown)
 
 . /root/bin/_script_header.sh
+
 if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
 fi
 
 if [ `mountpoint -q /encrypted ; echo $?` -ne 0 ] ; then 
-  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
 else
-  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
 fi
 
 exit
 #####
 # new crontab entry
 
-6 * * * * /root/bin/sprawdz-czy-encrypted-jest-zamontowany.sh
+6 * * * * sleep $((RANDOM \% 60)) && /root/bin/sprawdz-czy-encrypted-jest-zamontowany.sh
