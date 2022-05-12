d43895fd551eafa9655c24892061b5a2236f2704
root <pmatuszy.login+github@matuszyk.com>
2022-05-12 21:53:47 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2022.05.12 21:53:47"

--- update-rclone.sh
+++ update-rclone.sh
@@ -8,18 +8,24 @@ if [ -f "$HEALTHCHECKS_FILE" ];then
 fi
 
 export RCLONE_BIN=$(type -fP rclone)
-if pgrep -f "${RCLONE_BIN}" > /dev/null ; then
-  m=$(
-  echo '#####################################################'
-  echo '#####################################################'
-  echo
-  echo "${RCLONE_BIN} dziala, wiec nie startuje nowej instancji a po prostu koncze dzialanie skryptu"
-  echo
-  echo '#####################################################'
-  echo '#####################################################' )
-  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
-  exit 1
-fi
+
+# nie badamy czy rclone jest uruchomiony, bo moze byc rclone mount i on jest zawsze uruchomiony. My updateujemy mimo to
+# nie badamy czy rclone jest uruchomiony, bo moze byc rclone mount i on jest zawsze uruchomiony. My updateujemy mimo to
+# nie badamy czy rclone jest uruchomiony, bo moze byc rclone mount i on jest zawsze uruchomiony. My updateujemy mimo to
+# nie badamy czy rclone jest uruchomiony, bo moze byc rclone mount i on jest zawsze uruchomiony. My updateujemy mimo to
+
+# if pgrep -f "${RCLONE_BIN}" > /dev/null ; then
+#   m=$(
+#   echo '#####################################################'
+#   echo '#####################################################'
+#   echo
+#   echo "${RCLONE_BIN} dziala, wiec nie startuje nowej instancji a po prostu koncze dzialanie skryptu"
+#   echo
+#   echo '#####################################################'
+#   echo '#####################################################' )
+#   /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
+#   exit 1
+# fi
 
 wersja_przed=$(echo " " ; echo " " ; echo "wersja przed: " ; "${RCLONE_BIN}" version 2>&1; echo " " )
 wersja_przed_short=$(echo " " ; echo " " ; echo "wersja: " ; "${RCLONE_BIN}" version|head -n 1 2>&1; echo " " )
