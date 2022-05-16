e1944a3e864f9307f8e39b3c10dae00b0648c109
root <pmatuszy.login+github@matuszyk.com>
2022-05-16 23:32:00 +0200

    "new push from backupss.eth.ss.matuszyk.com @ 2022.05.16 23:32:00"

--- sprawdz-czy-dziala-strona-yt2podcast.com.sh
+++ sprawdz-czy-dziala-strona-yt2podcast.com.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2022.05.16 - v. 0.3 - commented out sending emails sections
 # 2022.05.03 - v. 0.2 - added healthcheck support
 # 2021.xx.xx - v. 0.1 - initial release
 
@@ -13,7 +14,7 @@ if [ `wget yt2podcast.com:8080 -qO - |grep .xml|wc -l` -gt 0 ];then
    # echo dziala strona yt2podcast.com:8080 | strings | aha | /usr/bin/mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`hostname --short`) OK - strona yt2podcast.com:8080 dziala" matuszyk+`hostname`@matuszyk.com
    /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
 else 
-   echo "!!! NIE dziala strona yt2podcast.com:8080" | strings | aha | /usr/bin/mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`hostname --short`) PROBLEM - strona yt2podcast.com:8080 NIE dziala" matuszyk+`hostname`@matuszyk.com
+   # echo "!!! NIE dziala strona yt2podcast.com:8080" | strings | aha | /usr/bin/mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`hostname --short`) PROBLEM - strona yt2podcast.com:8080 NIE dziala" matuszyk+`hostname`@matuszyk.com
    /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
 fi
 
