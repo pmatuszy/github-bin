9f71733c529bd4793e6b276cb89300fbbf610625
root <pmatuszy.login+github@matuszyk.com>
2022-05-16 21:58:30 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2022.05.16 21:58:30"

--- HOST-SPECIFIC/backupche.eth.che.matuszyk.com/nagrywaj-BBC4.sh
+++ HOST-SPECIFIC/backupche.eth.che.matuszyk.com/nagrywaj-BBC4.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# 2022.05.16 - v. 0.6 - eliminacja curla by nie startowac "$url/start" 2x, poprawne badanie kodu powrotu ffmpeg przez dodanie exit $?
 # 2022.05.10 - v. 0.5 - dodalem obsluge healthchecks
 # 2022.02.04 - v. 0.4 - jak ffmpeg skonczy sie przedwczesnie to wprowadzilem opoznienie 60s, by nie podejmowac proby od razu po niepowodzeniu
 # 2022.01.30 - v. 0.3 - zmiana sprawdzania czy dzialamy interaktywnie
@@ -20,7 +21,6 @@ wlasciciel_pliku="che:che"
 
 if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
-  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL"/start 2>/dev/null
 fi
 
 secs_to_midnight=$((($(date -d "tomorrow 00:00" +%s)-$(date +%s))))
@@ -31,8 +31,7 @@ while (( $secs_to_midnight > 200 )) ; do
   DOKAD="${DOKAD_PREFIX}-`date '+%Y.%m.%d__%H%M%S'`.mp3"
   /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL"/start
 
-  m=$( ffmpeg -hide_banner -loglevel quiet -t "${secs_nagrywania}" -i "$SKAD" "$DOKAD" 2>&1)
-# ffmpeg -hide_banner -loglevel quiet -t "${secs_nagrywania}" -i "$SKAD" "$DOKAD"
+  m=$( ffmpeg -hide_banner -loglevel quiet -t "${secs_nagrywania}" -i "$SKAD" "$DOKAD" 2>&1 ; exit $?)
   kod_powrotu=$?
   chown "${wlasciciel_pliku}" "${DOKAD}"
   if (( $kod_powrotu == 0 ));then
@@ -46,7 +45,7 @@ if [ -z ${STY:-} ]; then    # checking if we are running interactively
 #  (echo "koniec wykonywania $0" && ls -lr `dirname "${DOKAD}"`) | strings | aha | \
 #      mailx -r root@`hostname` -a 'Content-Type: text/html' -s "$0 (`/bin/hostname`-`date '+%Y.%m.%d %H:%M:%S'`)" matuszyk@matuszyk.com
    log=$(echo;echo;echo "koniec wykonywania $0" && ls -lr `dirname "${DOKAD}"`;echo;echo)
-    /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$log" --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL"
+    /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$log $m" -o /dev/null "$HEALTHCHECK_URL"
 fi
 . /root/bin/_script_footer.sh
 
