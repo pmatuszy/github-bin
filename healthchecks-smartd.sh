3663119763afece525fbc54008d6fce460099587
root <pmatuszy.login+github@matuszyk.com>
2022-05-16 23:39:32 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2022.05.16 23:39:32"

---
+++ healthchecks-smartd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# 2022.05.16 - v. 0.1 - initial release
+
+. /root/bin/_script_header.sh
+
+if [ -f "$HEALTHCHECKS_FILE" ];then
+  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
+
+/usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
+. /root/bin/_script_footer.sh
+exit
+
+######
+template crontab entry:
+
+* * * * *   sleep $((RANDOM \% 60)) && /root/bin/healthchecks-ping.sh
