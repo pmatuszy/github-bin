9422c9b3d1dd40780c0ee8859c50604913ebfd43
root <pmatuszy.login+github@matuszyk.com>
2022-05-18 11:16:07 +0200

    "new push from lublin.eth.b.matuszyk.com @ 2022.05.18 11:16:07"

--- healthchecks-smartd.sh
+++ healthchecks-smartd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 2022.05.16 - v. 0.1 - initial release
+# 2022.05.18 - v. 0.1 - initial release
 
 . /root/bin/_script_header.sh
 
@@ -7,11 +7,22 @@ if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
 fi
 
-/usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
+
+m=$( echo " ";
+     systemctl restart smartd --no-pager  ; sleep 5 ; echo ; echo "STATUS PO RESTARCIE SERWISU: " ; echo
+     systemctl status smartd --no-pager ; echo )
+
+wiadomosc=""
+if [ $(echo "$m" |grep -i "error" | wc -l) -gt 0 ];then
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
+else
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
+fi
+
 . /root/bin/_script_footer.sh
 exit
 
 ######
 template crontab entry:
 
-* * * * *   sleep $((RANDOM \% 60)) && /root/bin/healthchecks-ping.sh
+0 18 * * *  sleep $((RANDOM \% 60)) && /root/bin/healthchecks-smartd.sh
