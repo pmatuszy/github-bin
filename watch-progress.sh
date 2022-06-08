6f4230b83bfe0a613810f435d6510f9eb2c63790
root <pmatuszy.login+github@matuszyk.com>
2022-06-08 17:20:32 +0200

    "new push from nucvm.eth.che.matuszyk.com @ 2022.06.08 17:20:32"

--- watch-progress.sh
+++ watch-progress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+# 2022.06.08 - v. 0.3 - dodalem par2 do monitorowanych komend
 # 2021.11.04 - v. 0.2 - zmiana watch na "progress -M"
 # 2021.09.19 - v. 0.1 - inicjalna wersja skryptu
 
-progress -M
-
+progress --monitor-continuously --additional-command par2 --wait --wait-delay 0.5
