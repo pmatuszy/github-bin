fe4dcd3825447bb205ea4c9727939e3b8f201540
root <pmatuszy.login+github@matuszyk.com>
2022-06-15 08:02:36 +0200

    "new push from secureuploadserver.eth.r.matuszyk.com @ 2022.06.15 08:02:36"

---
+++ HOST-SPECIFIC/secureuploadserver.eth.r.matuszyk.com/zamontuj-win-disks-DivX.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# 2021.04.20 - v. 0.1 - zmiana, by bylo jedno tylko pytanie o haslo
+# 2020.xx.xx - v. 0.1 - initial release
+
+. /root/bin/_script_header.sh
+
+loc_dir_name="/mnt/rsync-master-DivX"
+rem_dir_name="//laptopvm.eth.b.matuszyk.com/DivX"
+
+echo 
+echo "skad  : $rem_dir_name"
+echo "dokad : $loc_dir_name"
+echo 
+
+read -p "Wpisz haslo: " -s PASSWD
+
+umount "${loc_dir_name}" 2>/dev/null  # just in case sa zamontowane, by nie dostawac komunikatu "mount error(16): Device or resource busy"
+
+mount.cifs -o user=p,password=$PASSWD "${rem_dir_name}" "${loc_dir_name}" 
+df -hP "${loc_dir_name}"
+
+. /root/bin/_script_footer.sh
