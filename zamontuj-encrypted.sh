9623ee01ad47852fe214dacd586ad3d3e6acaccb
root <pmatuszy.login+github@matuszyk.com>
2022-06-21 10:06:35 +0200

    "new push from yt2podcast @ 2022.06.21 10:06:35"

--- zamontuj-encrypted.sh
+++ zamontuj-encrypted.sh
@@ -1,5 +1,12 @@
+# 2022.06.21 - v. 0.3 - dodalem obsluge healthcheckow
 # 2021.09.19 - v. 0.2 - dodana funkcja fsck, czytanie hasla do zmiennej
 # 2021.01.30 - v. 0.1 - initial release (date unknown)
+
+. /root/bin/_script_header.sh
+
+if [ -f "$HEALTHCHECKS_FILE" ];then
+  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
 
 echo
 read -r -p "Wpisz haslo: " -s PASSWD
@@ -31,6 +38,17 @@ echo -n "$PASSWD" | cryptsetup luksOpen ${nazwa_pliku} encrypted_luks_file_in_ro
 zrob_fsck /dev/mapper/encrypted_luks_file_in_root
 
 mount -o noatime /dev/mapper/encrypted_luks_file_in_root /encrypted
+kod_powrotu=$?
+if [ $kod_powrotu -ne 0 ]; then
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
+  exit $kod_powrotu # cos poszlo nie tak
+else
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
+fi
 
 df -h /encrypted
 echo
+
+. /root/bin/_script_footer.sh
+exit
+
