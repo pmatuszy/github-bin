654da9bcd04e502c5169f51fcba7beadb37c34c9
root <pmatuszy.login+github@matuszyk.com>
2022-06-24 14:34:07 +0200

    "new push from zurich.eth.che.matuszyk.com @ 2022.06.24 14:34:07"

--- HOST-SPECIFIC/zurich.eth.che.matuszyk.com/zamontuj-encrypted-zurich.sh
+++ HOST-SPECIFIC/zurich.eth.che.matuszyk.com/zamontuj-encrypted-zurich.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# 2022.06.24 - v. 0.2 - dodano obsluge healthcheckow i grep -v grep 
 # 2021.01.30 - v. 0.1 - initial release (date unknown)
 
 df -h /encrypted
@@ -16,6 +19,13 @@ echo startuje vpnserver
 /encrypted/vpnserver/vpnserver start
 
 echo ; echo
-ps -ef |grep vpnserver
+ps -ef |grep vpnserver | grep -v grep
 echo ; echo
 
+if [ -x /root/bin/sprawdz-czy-encrypted-jest-zamontowany.sh ];then
+  /root/bin/sprawdz-czy-encrypted-jest-zamontowany.sh
+fi
+
+if [ -x /root/bin/sprawdz-czy-dziala-server-vpn.sh ];then
+  /root/bin/sprawdz-czy-dziala-server-vpn.sh
+fi
