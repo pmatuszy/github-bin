2e62825cc46a2ea73e7136ff4bb9f7b49ce16338
root <pmatuszy.login+github@matuszyk.com>
2022-08-09 09:24:37 +0200

    "new push from secureuploadserver.eth.r.matuszyk.com @ 2022.08.09 09:24:37"

---
+++ healthchecks-geoipupdate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# 2022.07.04 - v. 0.1 - initial release
+
+. /root/bin/_script_header.sh
+
+if [ -f "$HEALTHCHECKS_FILE" ];then
+  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
+
+m=$( echo " "; echo "aktualna data: `date '+%Y.%m.%d %H:%M'`" ; echo ; /usr/bin/geoipupdate -v 2>&1; exit $?)
+kod_powrotu=$?
+
+if [ $kod_powrotu -ne 0 ]; then
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
+  exit $kod_powrotu # cos poszlo nie tak
+else
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
+fi
+
+. /root/bin/_script_footer.sh
+
+exit
+#####
+# new crontab entry
+
+5 7 * * * sleep $((RANDOM \% 50)) && /root/bin/healthchecks-geoipupdate.sh
