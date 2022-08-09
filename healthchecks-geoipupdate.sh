296b235af9ad01c24ecd257eaf53ff5ab4641955
root <pmatuszy.login+github@matuszyk.com>
2022-08-09 09:27:43 +0200

    "new push from www02.eth.r.matuszyk.com @ 2022.08.09 09:27:43"

--- healthchecks-geoipupdate.sh
+++ healthchecks-geoipupdate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# 2022.08.09 - v. 0.2 - added info about script version to the output 
 # 2022.07.04 - v. 0.1 - initial release
 
 . /root/bin/_script_header.sh
@@ -7,7 +8,10 @@ if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
 fi
 
-m=$( echo " "; echo "aktualna data: `date '+%Y.%m.%d %H:%M'`" ; echo ; /usr/bin/geoipupdate -v 2>&1; exit $?)
+m=$( echo " "; echo "aktualna data: `date '+%Y.%m.%d %H:%M'`" ; echo ; 
+     cat  $0|grep -e '2022'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}' ; echo ; echo
+     /usr/bin/geoipupdate -v 2>&1; exit $?
+   )
 kod_powrotu=$?
 
 if [ $kod_powrotu -ne 0 ]; then
