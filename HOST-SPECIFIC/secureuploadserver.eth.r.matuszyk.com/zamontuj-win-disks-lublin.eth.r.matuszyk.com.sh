81c57b0c8da89b1abcad2e9b8bf3b1f2e668014f
root <pmatuszy.login+github@matuszyk.com>
2022-10-10 16:55:59 +0200

    "new push from secureuploadserver.eth.r.matuszyk.com @ 2022.10.10 16:55:59"

--- HOST-SPECIFIC/secureuploadserver.eth.r.matuszyk.com/zamontuj-win-disks-lublin.eth.r.matuszyk.com.sh
+++ HOST-SPECIFIC/secureuploadserver.eth.r.matuszyk.com/zamontuj-win-disks-lublin.eth.r.matuszyk.com.sh
@@ -14,18 +14,18 @@ echo  "remote_server = $remote_server"
 echo 
 echo 
 
-#set -x
-echo ; echo "w KeePassie:" ; echo  "Samba (p @ lublin.eth.b.matuszyk.com)"
+set -x
+echo ; echo "w KeePassie:" ; echo  "Samba (p @ lublin.eth.r.matuszyk.com)"
 
 read -p "Wpisz haslo: " -s PASSWD ; echo 
 
 loc_dir_name="/mnt/rsync-master-archiwum"
-rem_dir_name="//lublin.eth.b.matuszyk.com/archiwum-MASTER-SOURCE_read_only"
+rem_dir_name="//lublin.eth.r.matuszyk.com/archiwum-MASTER-SOURCE_read_only"
 
 umount "${loc_dir_name}" 2>/dev/null  # just in case sa zamontowane, by nie dostawac komunikatu "mount error(16): Device or resource busy"
 mount.cifs -o user=p,password=$PASSWD "${rem_dir_name}" "${loc_dir_name}"
 
-df -hP |egrep 'Filesystem|lublin.eth.b.matuszyk.com'
+df -hP |egrep 'Filesystem|lublin.eth.r.matuszyk.com'
 
 # set +x
 
