0ea2308539a24ef1bdab548c6c7b3b18bd8fd1a1
root <pmatuszy.login+github@matuszyk.com>
2022-10-12 10:59:48 +0200

    "new push from lublin.eth.r.matuszyk.com @ 2022.10.12 10:59:47"

--- HOST-SPECIFIC/lublin.eth.r.matuszyk.com/zamontuj-encrypted-lublin.sh
+++ HOST-SPECIFIC/lublin.eth.r.matuszyk.com/zamontuj-encrypted-lublin.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
+# 2022.10.11 - v  0.2 - added healthcheck support 
 # 2022.07.30 - v. 0.1 - initial release (date unknown)
+
+. /root/bin/_script_header.sh
+
+if [ -f "$HEALTHCHECKS_FILE" ];then
+  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
 
 echo
 read -r -p "Wpisz haslo: " -s PASSWD
@@ -25,14 +31,26 @@ fsck $1
 }
 ################################################################################
 
+
+nazwa_pliku=/encrypted.luks2
+
+echo
+echo '########## /encrypted.luks2 ==> /encrypted'
+echo
+echo -n "$PASSWD" | cryptsetup luksOpen ${nazwa_pliku} encrypted_luks_file_in_root -d -
+zrob_fsck /dev/mapper/encrypted_luks_file_in_root
+mount -o noatime /dev/mapper/encrypted_luks_file_in_root /encrypted
+
 echo
 echo '########## /dev/vg_crypto_buffalo1/lv_do_luksa_buffalo1 ==> /mnt/luks-buffalo1'
 echo
 echo -n "$PASSWD" | cryptsetup luksOpen /dev/vg_crypto_buffalo1/lv_do_luksa_buffalo1 luks-on-lv-buffalo1 -d -
 
 zrob_fsck /dev/vg_crypto_buffalo1/lv_do_luksa_buffalo1
-
 mount -o noatime /dev/mapper/luks-on-lv-buffalo1 /mnt/luks-buffalo1
 
+df -h /encrypted
+echo
 df -h /mnt/luks-buffalo1
+
 
