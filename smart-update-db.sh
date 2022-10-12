e265e81d46777d0726a33bb7a949a7b3950d9c6d
root <pmatuszy.login+github@matuszyk.com>
2022-10-12 10:44:45 +0200

    "new push from lublin @ 2022.10.12 10:44:45"

--- smart-update-db.sh
+++ smart-update-db.sh
@@ -14,7 +14,7 @@ if [ ! -z ${STY:-} ]; then    # checking if we are running within screen
   echo -ne "${tcScrTitleStart}${0}${tcScrTitleEnd}"
 fi
 
-echo ; echo ; cat  $0|grep -e '2022'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}'
+echo ; echo ; cat  $0|grep -e '2022'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}'; echo
 
 echo ; echo
 update-smart-drivedb
