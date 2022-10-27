a412d68069fdf4bff963f02336e13d800460197e
root <pmatuszy.login+github@matuszyk.com>
2022-10-27 11:01:09 +0200

    "new push from zurich.eth.che.matuszyk.com @ 2022.10.27 11:01:09"

--- jd.sh
+++ jd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# 2022.10.27 - v. 0.6 - bugfixes, added support for Raspbian, small changes in print format output,added printing of the script version
 # 2022.10.11 - v. 0.5 - discovery if it is x86 or raspberry pi
 # 2021.04.16 - v. 0.4 - checking if it is redhat system
 # 2020.11.27 - v. 0.3 - added printing of the disk serial numbers
@@ -14,6 +15,8 @@ fi
 
 echo 
 
+cat  $0|grep -e '2022'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}' ; echo 
+
 if [[ `cat /etc/os-release /etc/redhat-release 2>/dev/null |grep -qi centos ; echo $?` == 0 ]] ; then
   echo CENTOS | boxes -s 40x5 -a c ; echo 
   for p in `fdisk -l|grep 'Disk /dev'|egrep -v 'mapper|/md|/ram|mmcb|/dev/loop'|sort|tr -d :`;do
@@ -28,13 +31,13 @@ hardware_type=""
 if [[ $(uname --machine) == "x86_64" ]] ; then
    hardware_type="Intel"
 fi
-if [[ $(uname --machine) == "aarch64" ]] ; then
+if [[ $(uname --machine) == "aarch64" || $(uname --machine) == "armv7l" ]] ; then
    hardware_type="PI"
 fi
 
-if [[ `cat /etc/os-release /etc/redhat-release 2>/dev/null |grep -qi ubuntu ; echo $?` == 0 ]] ; then
-  echo $hardware_type | boxes -s 40x5 -a c ; echo 
-  
+echo $hardware_type | boxes -s 40x5 -a c ; echo 
+
+if [[ `cat /etc/os-release /etc/redhat-release 2>/dev/null |egrep -qi "ubuntu|Raspbian" ; echo $?` == 0 ]] ; then
   for p in `fdisk -l|grep 'Disk /dev'|egrep -v 'mapper|/md|/ram|mmcb|/dev/loop'|awk '{print $2}'|sort|tr -d :`;do
      echo `gdisk -l $p |grep "Disk /dev"` "   " `hdparm -I $p 2>/dev/null | grep 'Serial\ Number'`
   done
