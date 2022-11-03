9ba1aaa1d51c016f78e3494dce61d34b14a529b9
root <pmatuszy.login+github@matuszyk.com>
2022-11-03 19:07:38 +0100

    "new push from lublin.eth.r.matuszyk.com @ 2022.11.03 19:07:38"

--- sprawdz-czy-reboot-required.sh
+++ sprawdz-czy-reboot-required.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
-# 2022.05.03 - v. 0.1 - initial release (date unknown)
+# 2022.11.03 - v. 0.1 - initial release (date unknown)
 
 . /root/bin/_script_header.sh
 if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
 fi
 
-# spr. czy dziala vpn
-if [ `ps -ef|grep vpnserver | awk '{print $8}'|grep -v grep|uniq|wc -l` -eq 0 ];then 
-#  (echo "vpn na `hostname` NIE dziala" | mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`/bin/hostname`-`date '+\%Y.\%m.\%d \%H:\%M:\%S'`) vpn nie dziala" matuszyk+`/bin/hostname`@matuszyk.com)
+# spr. czy potrzebny jest reboot
+if [ -f /var/run/reboot-required ];then 
   /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
 else
   /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
 fi
 
 exit
+
 #####
 # new crontab entry
 
-*/3 * * * * /root/bin/sprawdz-czy-dziala-server-vpn.sh
-
-# old crontab entry
-# spr. czy dziala vpn
-#5 */6 * * * if [ `ps -ef|grep vpnserver | awk '{print $8}'|grep -v grep|uniq|wc -l` -eq 0 ];then (echo "vpn na `hostname` NIE dziala" | mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`/bin/hostname`-`date '+\%Y.\%m.\%d \%H:\%M:\%S'`) vpn nie dziala" matuszyk+`/bin/hostname`@matuszyk.com) ; fi
-
-~
+*/3 * * * * /root/bin/sprawdz-czy-reboot-required.sh
 
