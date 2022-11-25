3aea3a2bfbd8666442a9976188b35ac79a629137
root <pmatuszy.login+github@matuszyk.com>
2022-11-25 12:41:34 +0100

    "new push from lublin.eth.r.matuszyk.com @ 2022.11.25 12:41:34"

--- dmesg-tail.sh
+++ dmesg-tail.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# 2020.11.25 - v. 0.2 - added figlet
 # 2020.11.11 - v. 0.1 - initial release
+
+figlet -w 120 kernel logs
+sleep 1
 
 dmesg -wT --color=never
--- journal-tail.sh
+++ journal-tail.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # 2022.11.25 - v. 0.1 - initial release
 
+figlet -w 120 logs from systemd
+sleep 1 
+
 /usr/bin/journalctl -fan500
 
 #  -f --follow                Follow the journal
