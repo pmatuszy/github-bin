79da8c410222c1d1088bd6a8271ab6d8e38df22c
root <pmatuszy.login+github@matuszyk.com>
2022-11-25 12:27:45 +0100

    "new push from lublin.eth.r.matuszyk.com @ 2022.11.25 12:27:45"

---
+++ journal-tail.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 2022.11.25 - v. 0.1 - initial release
+
+/usr/bin/journalctl -fan500
+
+#  -f --follow                Follow the journal
+#  -a --all                   Show all fields, including long and unprintable
+#  -n --lines[=INTEGER]       Number of journal entries to show
+
