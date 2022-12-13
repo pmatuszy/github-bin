76cff9c4639916b019fbbe8cd168e8dbd4c4c7cd
root <root@nuci7b.eth.che.matuszyk.com>
2022-12-13 09:46:19 +0100

    "new push from nuci7b @ 2022.12.13 09:46:19"

--- git-pull.sh
+++ git-pull.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# 2022.12.13 - v. 0.6 - hostname specified with start (works with or without domainaname)
 # 2022.09.30 - v. 0.5 - checking if we have access to remote repo
 # 2022.05.18 - v. 0.4 - added removing git-pull.sh and gill-push.sh from $HOME/bin
 #                       added set -o options at the beginning
@@ -8,6 +11,9 @@
 # exit when your script tries to use undeclared variables
 set -o nounset
 set -o pipefail
+
+echo
+cat  $0|grep -e '# *20[123][0-9]'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}' ; echo ; echo
 
 echo
 echo "Do you want to do kind of git pull and configure local scripts? [y/N]"
@@ -30,8 +36,8 @@ if [ "${p}" == 'y' -o  "${p}" == 'y' ]; then
   rm -rf $HOME/github-bin/.git
   git clone git+ssh://git@github.com/pmatuszy/github-bin.git
   cp -v github-bin/* $HOME/bin
-  cp -v github-bin/HOST-SPECIFIC/`hostname`/* $HOME/bin
-  cp -v github-bin/HOST-SPECIFIC/`hostname`/.[a-zA-Z0-9]* $HOME   2>/dev/null
+  cp -v github-bin/HOST-SPECIFIC/`hostname`*/* $HOME/bin
+  cp -v github-bin/HOST-SPECIFIC/`hostname`*/.[a-zA-Z0-9]* $HOME   2>/dev/null
   rm $HOME/bin/git-pull.sh $HOME/bin/git-push.sh
   cd github-bin
   git status
