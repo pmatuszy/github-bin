0cc379b33818f43775105daf36c6a637190ebc47
root <pmatuszy.login+github@matuszyk.com>
2022-12-14 10:55:22 +0100

    "new push from backupche.eth.che.matuszyk.com @ 2022.12.14 10:55:22"

--- HOST-SPECIFIC/backupche.eth.che.matuszyk.com/nagrywaj-kijow.sh
+++ HOST-SPECIFIC/backupche.eth.che.matuszyk.com/nagrywaj-kijow.sh
@@ -9,8 +9,7 @@ wlasciciel_pliku="che:che"
 
 while : ; do 
   nazwa_pliku="Kijow-livecam_$(date '+%Y%m%d_%H%M%S').mp4"
-  echo /usr/local/bin/youtube-dl --ignore-errors --no-part --output "${DOKAD_PREFIX}/${nazwa_pliku}" "$url"
-  /usr/local/bin/youtube-dl -hide_banner --ignore-errors --no-part --output "${DOKAD_PREFIX}/${nazwa_pliku}" $url 
+  /usr/local/bin/youtube-dl --ignore-errors --no-part --output "${DOKAD_PREFIX}/${nazwa_pliku}" "$url" 
   chown "${wlasciciel_pliku}" "${DOKAD_PREFIX}/${nazwa_pliku}" 2>/dev/null
   (echo "koniec wykonywania $0" && ls -lr `dirname "${DOKAD_PREFIX}"`) | strings | aha | \
       mailx -r root@`hostname` -a 'Content-Type: text/html' -s "$0 (`/bin/hostname`-`date '+%Y.%m.%d %H:%M:%S'`)" matuszyk@matuszyk.com
