503cec9a8018cdbf32103f92f289afff6f008ef5
root <pmatuszy.login+github@matuszyk.com>
2022-12-14 10:39:32 +0100

    "new push from backupche.eth.che.matuszyk.com @ 2022.12.14 10:39:32"

--- HOST-SPECIFIC/backupche.eth.che.matuszyk.com/nagrywaj-kijow.sh
+++ HOST-SPECIFIC/backupche.eth.che.matuszyk.com/nagrywaj-kijow.sh
@@ -7,7 +7,7 @@ export DOKAD_PREFIX="/worek-samba/nagrania/Kijow-webcamy"
 wlasciciel_pliku="che:che"
 
 while : ; do 
-  nazwa_pliku=$(Kijow-livecam_$(date '+%Y%m%d_%H%M%S').mp4)
+  nazwa_pliku="$Kijow-livecam_$(date '+%Y%m%d_%H%M%S').mp4"
   /usr/local/bin/youtube-dl -hide_banner --ignore-errors --no-part --output "${DOKAD_PREFIX}/${nazwa_pliku}" $url 
   chown "${wlasciciel_pliku}" "${DOKAD_PREFIX}/${nazwa_pliku}" 2>/dev/null
   (echo "koniec wykonywania $0" && ls -lr `dirname "${DOKAD_PREFIX}"`) | strings | aha | \
