85d65dd0f12e4cd9f9311035e0c9d449a439378b
root <pmatuszy.login+github@matuszyk.com>
2022-12-20 15:12:27 +0100

    "new push from www02.eth.r.matuszyk.com @ 2022.12.20 15:12:27"

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -6,10 +6,6 @@ if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
 fi
 
-echo basename $0
-echo $HEALTHCHECKS_FILE
-echo $HEALTHCHECK_URL
-
 export BACKUP_DESTINATION=${BACKUP_DESTINATION:-"/var/www/$(date '+%Y.%m.%d_%H%M%S')_cr_mysqldump-all-databases.sql.pbz2"}
 export MYSQL_USER=${MYSQL_USER:-root}
 export MAX_RANDOM_DELAY_IN_SEC=${MAX_RANDOM_DELAY_IN_SEC:-50}
@@ -20,28 +16,23 @@ if (( $? != 0 )); then      # we set RANDOM_DELAY only when running NOT from ter
   sleep $RANDOM_DELAY
 fi
 
-SCRIPT_VERSION=$(echo ; cat  $0|grep -e '# *20[123][0-9]'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}' ; echo ; echo)
+SCRIPT_VERSION=$(echo ; cat $0|grep -e '# *20[123][0-9]'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}' ;  echo " "; echo "aktualna data: `date '+%Y.%m.%d %H:%M'`" ; echo ; echo)
 
-/usr/bin/mysqlshow -u "${MYSQL_USER}" 2>&1 > /dev/null
+/usr/bin/mysqlshow -u "${MYSQL_USER}" > /dev/null 2>&1
 if (( $? != 0 )); then
    m="I can't connect to MySQL server using mysqlshow "
-   wiadomosc="$SCRIPT_VERSION $m"
+   wiadomosc=$(echo -e "$SCRIPT_VERSION\n\n$m")
    /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 --data-raw "$wiadomosc" -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
-   echo tutaj
    exit 2
 fi
-m=$(/usr/bin/nice -19 /usr/bin/mysqldump -u ${MYSQL_USER} --verbose --all-databases | /usr/bin/nice -19 /usr/bin/pbzip2 -9qc -p2 > "${BACKUP_DESTINATION}"  2>&1 ; exit $?) 
-
-exit
-
-
-
-
-
-
+m=$( { /usr/bin/nice -19 /usr/bin/mysqldump -u ${MYSQL_USER} --verbose --all-databases | /usr/bin/nice -19 /usr/bin/pbzip2 -9qc -p2 > "${BACKUP_DESTINATION}" ; } 2>&1 ; exit $?)
 kod_powrotu=$?
 
-wiadomosc="$SCRIPT_VERSION $m"
+
+WHAT_DATABASES_WE_HAVE=$(/usr/bin/mysqlshow -u "${MYSQL_USER}")
+OUTPUT_FILE=$(ls -l ${BACKUP_DESTINATION})
+ALL_BACKUP_FILES=$(ls -l $(dirname ${BACKUP_DESTINATION}))
+wiadomosc=$(echo -e "$SCRIPT_VERSION\n\n$WHAT_DATABASES_WE_HAVE\n\n$m\n\n$OUTPUT_FILE\n\nALL BACKUP FILES:\n$ALL_BACKUP_FILES")
 
 if (( $kod_powrotu != 0 )); then
   /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 --data-raw "$wiadomosc" -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
