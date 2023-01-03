b1ab5471e0fcaf7e04fd1902334a8ae38c91382a
root <root@nuci7b.eth.che.matuszyk.com>
2023-01-03 13:49:08 +0100

    "new push from nuci7b.eth.r.matuszyk.com @ 2023.01.03 13:49:08"

--- sprawdz-czy-dziala-server-vpn.sh
+++ sprawdz-czy-dziala-server-vpn.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+
+# 2023.01.03 - v. 0.2 - dodano random delay jesli skrypt jest wywolywany nieinteraktywnie
 # 2022.05.03 - v. 0.1 - initial release (date unknown)
 
 . /root/bin/_script_header.sh
 if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
+
+export MAX_RANDOM_DELAY_IN_SEC=${MAX_RANDOM_DELAY_IN_SEC:-50}
+tty 2>&1 >/dev/null
+if (( $? != 0 )); then      # we set RANDOM_DELAY only when running NOT from terminal
+  export RANDOM_DELAY=$((RANDOM % $MAX_RANDOM_DELAY_IN_SEC ))
+  sleep $RANDOM_DELAY
+else
+  echo ; echo "Interactive session detected: I will NOT introduce RANDOM_DELAY..."
 fi
 
 # spr. czy dziala vpn
--- sprawdz-czy-encrypted-jest-zamontowany.sh
+++ sprawdz-czy-encrypted-jest-zamontowany.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-# 2022.05.12 - v. 0.1 - usunieta zmienna m, ktora nie byla uzywana
+
+# 2023.01.03 - v. 0.3 - dodano random delay jesli skrypt jest wywolywany nieinteraktywnie
+# 2022.05.12 - v. 0.2 - usunieta zmienna m, ktora nie byla uzywana
 # 2022.05.03 - v. 0.1 - initial release (date unknown)
 
 . /root/bin/_script_header.sh
 
 if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
+
+export MAX_RANDOM_DELAY_IN_SEC=${MAX_RANDOM_DELAY_IN_SEC:-50}
+tty 2>&1 >/dev/null
+if (( $? != 0 )); then      # we set RANDOM_DELAY only when running NOT from terminal
+  export RANDOM_DELAY=$((RANDOM % $MAX_RANDOM_DELAY_IN_SEC ))
+  sleep $RANDOM_DELAY
+else
+  echo ; echo "Interactive session detected: I will NOT introduce RANDOM_DELAY..."
 fi
 
 if [ `mountpoint -q /encrypted ; echo $?` -ne 0 ] ; then 
