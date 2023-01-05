418a0961337ae14ae50b2c0e7a22542fe5a93fb1
root <pmatuszy.login+github@matuszyk.com>
2023-01-05 12:00:14 +0100

    "new push from zurich.eth.che.matuszyk.com @ 2023.01.05 12:00:14"

--- HOST-SPECIFIC/zurich.eth.che.matuszyk.com/zamontuj-encrypted-zurich.sh
+++ HOST-SPECIFIC/zurich.eth.che.matuszyk.com/zamontuj-encrypted-zurich.sh
@@ -1,20 +1,87 @@
 #!/bin/bash
 
+# 2023.01.05 - v. 0.3 - a lot of changes - too many to describe here :-)
 # 2022.06.24 - v. 0.2 - dodano obsluge healthcheckow i grep -v grep 
 # 2021.01.30 - v. 0.1 - initial release (date unknown)
 
-df -h /encrypted
+. /root/bin/_script_header.sh
 
-nazwa_pliku=/encrypted.luks2
+cat  $0|grep -e '2022'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}' ; echo ; echo
 
-cryptsetup luksOpen ${nazwa_pliku} encrypted_luks_file_in_root
+if [ -f "$HEALTHCHECKS_FILE" ];then
+  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
 
-mount -o noatime /dev/mapper/encrypted_luks_file_in_root /encrypted
-
-df -h /encrypted
+echo
+read -r -p "Wpisz haslo: " -s PASSWD
 echo
 
-echo startuje vpnserver
+################################################################################
+zrob_fsck() {
+################################################################################
+echo ; echo "==> ########## zrob_fsck($1)"
+
+echo czas na fsck $1 ...
+
+if [ $(lsblk -no FSTYPE /dev/mapper/encrypted_luks_device_encrypted.luks2) == 'ext4' ];then
+  fsck.ext4 -f $1
+else
+  fsck      -C -M -R -T $1
+fi
+
+kod_powrotu=$?
+echo "kod powrotu z fsck to $kod_powrotu (przebieg 1-szy)"
+
+if (( $kod_powrotu != 0 ));then
+  echo
+  echo ... and once again fsck
+  echo
+
+  if [ $(lsblk -no FSTYPE /dev/mapper/encrypted_luks_device_encrypted.luks2) == 'ext4' ];then
+    fsck.ext4 -f $1
+  else
+    fsck      -C -M -R -T $1
+  fi
+  echo "kod powrotu z fsck to $? (przebieg 2-gi)"
+else
+  echo "fsck zrobiony"
+fi
+echo "<== ########## zrob_fsck($1)"
+}
+################################################################################
+zamontuj_fs_MASTER() {
+echo ; echo "==> ########## zamontuj_fs_MASTER($1, $2, $3)"
+
+if [ $(mountpoint -q $2 ; echo $?) -eq 0 ] ; then
+   echo $1 jest juz zamontowany ... wychodze
+   echo "<== ########## zamontuj_fs_MASTER($1, $2, $3)"
+   return
+fi
+
+echo -n "$PASSWD" | cryptsetup luksOpen "${1}" encrypted_luks_device_"$(basename ${1})" -d -
+
+if (( $? != 0 ));then
+  echo  ; echo "NIE MOGE ZAMONTOWAC $1 pod $2 !!!!!!!"; echo "wychodze ..."
+  echo "<== ########## zamontuj_fs_MASTER($1, $2, $3)"
+  return
+fi
+
+zrob_fsck /dev/mapper/encrypted_luks_device_"$(basename ${1})"
+mount -o $3 /dev/mapper/encrypted_luks_device_"$(basename ${1})" "${2}"
+
+echo "<== ########## zamontuj_fs_MASTER($1, $2, $3)"
+}
+################################################################################
+
+vgchange -a y
+sleep 1
+
+zamontuj_fs_MASTER /encrypted.luks2                                /encrypted            noatime
+zamontuj_fs_MASTER /dev/vg_crypto_20221114_DyskD/lv_20221114_DyskD /mnt/luks-lv-icybox-A noatime,data=writeback,barrier=0,nobh,errors=remount-ro
+
+df -h /encrypted /mnt/luks-lv-icybox-A
+
+echo ; echo startuje vpnserver
 
 /encrypted/vpnserver/vpnserver start
 
@@ -22,10 +89,5 @@ echo ; echo
 ps -ef |grep vpnserver | grep -v grep
 echo ; echo
 
-if [ -x /root/bin/sprawdz-czy-encrypted-jest-zamontowany.sh ];then
-  /root/bin/sprawdz-czy-encrypted-jest-zamontowany.sh
-fi
-
-if [ -x /root/bin/sprawdz-czy-dziala-server-vpn.sh ];then
-  /root/bin/sprawdz-czy-dziala-server-vpn.sh
-fi
+/root/bin/sprawdz-czy-encrypted-jest-zamontowany.sh
+/root/bin/sprawdz-czy-dziala-server-vpn.sh
