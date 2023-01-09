9d155b90ab5a4f1f470ec8939eec55e81a25e836
root <pmatuszy.login+github@matuszyk.com>
2023-01-09 09:25:36 +0100

    "new push from secureuploadserver.eth.r.matuszyk.com @ 2023.01.09 09:25:36"

--- healthchecks-geoipupdate.sh
+++ healthchecks-geoipupdate.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# 2023.01.09 - v. 0.3 - added random delay support
 # 2022.08.09 - v. 0.2 - added info about script version to the output 
 # 2022.07.04 - v. 0.1 - initial release
 
@@ -6,6 +8,15 @@
 
 if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
+
+export MAX_RANDOM_DELAY_IN_SEC=${MAX_RANDOM_DELAY_IN_SEC:-50}
+tty 2>&1 >/dev/null
+if (( $? != 0 )); then      # we set RANDOM_DELAY only when running NOT from terminal
+  export RANDOM_DELAY=$((RANDOM % $MAX_RANDOM_DELAY_IN_SEC ))
+  sleep $RANDOM_DELAY
+else
+  echo ; echo "Interactive session detected: I will NOT introduce RANDOM_DELAY..."
 fi
 
 m=$( echo " "; echo "aktualna data: `date '+%Y.%m.%d %H:%M'`" ; echo ; 
