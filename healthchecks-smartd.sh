a9f79f3fad63ff2bf0a0d64fb42d414446244796
root <pmatuszy.login+github@matuszyk.com>
2023-01-16 18:56:00 +0100

    "new push from laptopvm.eth.b.matuszyk.com @ 2023.01.16 18:56:00"

--- healthchecks-smartd.sh
+++ healthchecks-smartd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2023.01.16 - v. 0.3 - better service restart failure detection, redirection of 2> on 1>
 # 2023.01.03 - v. 0.2 - dodano random delay jesli skrypt jest wywolywany nieinteraktywnie
 # 2022.05.18 - v. 0.1 - initial release
 
@@ -20,11 +21,14 @@ fi
 
 m=$( echo ; echo "aktualna data: `date '+%Y.%m.%d %H:%M'`" ; echo ; echo ;
      cat  $0|grep -e '# *20[123][0-9]'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}' ; echo ; echo
-     systemctl restart smartd --no-pager  ; sleep 5 ; echo ; echo "STATUS PO RESTARCIE SERWISU: " ; echo
-     systemctl status smartd --no-pager ; echo )
+     systemctl restart smartd --no-pager 2>&1
+     kod_powrotu=$?
+     sleep 5 ; echo ; echo "STATUS PO RESTARCIE SERWISU: " ; echo
+     systemctl status smartd --no-pager ; echo ; exit $kod_powrotu )
+wynik_po_restarcie=$?
 
 wiadomosc=""
-if [ $(echo "$m" |grep -i "error" | wc -l) -gt 0 ];then
+if (( $(echo "$m" |grep -i "error" | wc -l) > 0 )) || (( $wynik_po_restarcie != 0 )) ;then
   /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
 else
   /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
