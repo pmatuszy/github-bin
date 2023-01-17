c21760633dd5efe943cd7caf5549116b7e9d3d80
root <pmatuszy.login+github@matuszyk.com>
2023-01-17 08:28:07 +0100

    "new push from backupche.eth.che.matuszyk.com @ 2023.01.17 08:28:07"

--- sprawdz-czy-dziala-strona-yt2podcast.com.sh
+++ sprawdz-czy-dziala-strona-yt2podcast.com.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2023.01.17 - v. 0.5 - dodano random delay jesli skrypt jest wywolywany nieinteraktywnie
 # 2022.05.16 - v. 0.3 - commented out sending emails sections
 # 2022.05.03 - v. 0.2 - added healthcheck support
 # 2021.xx.xx - v. 0.1 - initial release
@@ -8,6 +9,15 @@
 
 if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
+
+export MAX_RANDOM_DELAY_IN_SEC=${MAX_RANDOM_DELAY_IN_SEC:-50}
+tty 2>&1 >/dev/null
+if (( $? != 0 )); then      # we set RANDOM_DELAY only when running NOT from terminal
+  export RANDOM_DELAY=$((RANDOM % $MAX_RANDOM_DELAY_IN_SEC ))
+  sleep $RANDOM_DELAY
+else
+  echo ; echo "Interactive session detected: I will NOT introduce RANDOM_DELAY..."
 fi
 
 if [ `wget yt2podcast.com:8080 -qO - |grep .xml|wc -l` -gt 0 ];then 
