4406f98845e96f4bec28bf335b202e860f46e166
root <pmatuszy.login+github@matuszyk.com>
2023-01-24 10:43:00 +0100

    "new push from bitcoinnetworknode.eth.che.matuszyk.com @ 2023.01.24 10:43:00"

---
+++ HOST-SPECIFIC/bitcoinnetworknode.eth.r.matuszyk.com/sprawdz-czy-dziala-bitcoin-daemon.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# 2023.01.24 - v. 0.1 - initial release
+
+. /root/bin/_script_header.sh
+if [ -f "$HEALTHCHECKS_FILE" ];then
+  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
+
+export MAX_RANDOM_DELAY_IN_SEC=${MAX_RANDOM_DELAY_IN_SEC:-50}
+tty 2>&1 >/dev/null
+if (( $? != 0 )); then      # we set RANDOM_DELAY only when running NOT from terminal
+  export RANDOM_DELAY=$((RANDOM % $MAX_RANDOM_DELAY_IN_SEC ))
+  sleep $RANDOM_DELAY
+else
+  echo ; echo "Interactive session detected: I will NOT introduce RANDOM_DELAY..."
+fi
+
+# spr. czy dziala demon bitcoina
+if [ $(pgrep -u bitcoin bitcoind -c) -eq 0 ];then 
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
+else
+  /usr/bin/curl -fsS -m 10 --retry 5 --retry-delay 5 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
+fi
+
+exit
+#####
+# new crontab entry
+
+@reboot ( sleep 60 && /root/bin/sprawdz-czy-dziala-bitcoin-daemon.sh ) 2>&1
+
+0 7-22 * * * /root/bin/sprawdz-czy-dziala-bitcoin-daemon.sh
