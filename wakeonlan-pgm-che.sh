9e733b5321978880676da7e1a481cbb70aa5513a
root <pmatuszy.login+github@matuszyk.com>
2023-01-30 09:06:53 +0100

    "new push from backupche.eth.che.matuszyk.com @ 2023.01.30 09:06:53"

--- wakeonlan-pgm-che.sh
+++ wakeonlan-pgm-che.sh
@@ -4,6 +4,12 @@
 
 . /root/bin/_script_header.sh
 
-wakeonlan -i 192.168.200.255 04:D9:F5:60:42:4A
+opoznienie=10
+
+for p in {1..3};do 
+  wakeonlan -i 192.168.200.255 04:D9:F5:60:42:4A
+  echo opoznienie $opoznienie
+  sleep $opoznienie
+done
 
 . /root/bin/_script_footer.sh
