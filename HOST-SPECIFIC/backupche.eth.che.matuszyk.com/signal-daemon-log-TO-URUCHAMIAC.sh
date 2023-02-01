b1e111f812eb4790adbfd1a81aed0eee404d624d
root <pmatuszy.login+github@matuszyk.com>
2023-02-01 19:21:53 +0100

    "new push from backupche.eth.che.matuszyk.com @ 2023.02.01 19:21:53"

---
+++ HOST-SPECIFIC/backupche.eth.che.matuszyk.com/signal-daemon-log-TO-URUCHAMIAC.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 2023.02.01 - v. 0.2 - added restart once a day
+# 202x.xx.xx - v. 0.1 - initial release (date unknown)
+
+. /root/bin/_script_header.sh
+
+czas_startu_skryptu=`date '+%s'
+secs_to_midnight=$((($(date -d "tomorrow 00:00" +%s)-$czas_startu_skryptu)))
+let max_timestamp_dzialania_skryptu=$((($(date +%s)+$secs_to_midnight+20)))
+opoznienie_miedzy_wywolaniami=5s
+
+while : ; do
+  let secs_nagrywania=secs_to_midnight+60
+
+  echo "[`date '+%Y.%m.%d %H:%M:%S'`] restart signala"
+  timeout --preserve-status --signal=HUP --kill-after=$((secs_nagrywania+120)) $((secs_nagrywania+60)) /opt/signal-cli/bin/signal-cli -u +41763691467 daemon 2>&1 > /encrypted/root/signal-output-`date '+%Y%m%d__%H_%M_%S'`.log
+
+  sleep $opoznienie_miedzy_wywolaniami # opozniamy bo jak sa problemy z siecia, to by nie startowac od razu z nastepna proba...
+  secs_to_midnight=$((($(date -d "tomorrow 00:00" +%s)-$(date +%s))))
+done
+
+. /root/bin/_script_footer.sh
--- HOST-SPECIFIC/backupche.eth.che.matuszyk.com/signal-daemon-log.sh
+++
@@ -1,5 +0,0 @@
-while : ; do 
-  echo "[`date '+%Y.%m.%d %H:%M:%S'`] restart signala"
-  /opt/signal-cli/bin/signal-cli -u +41763691467 daemon 2>&1 > /encrypted/root/signal-output-`date '+%Y%m%d__%H_%M_%S'`.log
-  echo 
-done
