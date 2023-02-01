c989e60599f53f1d84c35842c852a83958ca39ef
root <pmatuszy.login+github@matuszyk.com>
2023-02-01 08:57:40 +0100

    "new push from lublin.eth.r.matuszyk.com @ 2023.02.01 08:57:40"

--- smart-update-db.sh
+++ smart-update-db.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
+# 2023.02.01 - v. 0.2 - initial release
 # 2022.10.11 - v. 0.1 - initial release
 
-# exit when your script tries to use undeclared variables
-set -o nounset
-set -o pipefail
-# if we are run non-interactively - do not set the terminal title
-export tcScrTitleStart="\ek"
-export tcScrTitleEnd="\e\134"
-
-if [ ! -z ${STY:-} ]; then    # checking if we are running within screen
-  # I am setting the screen window title to the script name
-  echo -ne "${tcScrTitleStart}${0}${tcScrTitleEnd}"
-fi
+. /root/bin/_script_header.sh
 
 echo ; echo ; cat  $0|grep -e '# *20[123][0-9]'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}' ; echo
 
 echo
 update-smart-drivedb
 echo 
+
+. /root/bin/_script_footer.sh
