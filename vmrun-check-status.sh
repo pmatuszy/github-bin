0278de9b1e2c9af9ee4fd7a1e2434e8e55ae1b65
root <matuszyk@matuszyk.com>
2023-02-02 11:25:01 +0100

    "new push from nuci7 @ 2023.02.02 11:25:01"

--- vmrun-check-status.sh
+++ vmrun-check-status.sh
@@ -12,6 +12,59 @@ if (( $? != 0 )); then
   exit 1
 fi
 
+if [ -f "$HEALTHCHECKS_FILE" ];then
+  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
+
+if [ -f /root/SECRET/vmware-pass.sh ];then
+  . /root/SECRET/vmware-pass.sh
+fi
+
+# we set HEALTHCHECK_STATUS initially to OK - if anything goes wrong I will flip it to BAD
+export HEALTHCHECK_STATUS=OK
+
+#########################################################################################################
+spr_ip_address() {
+  if [ ! -z "${TPM_PASS:-}" ];then
+    address=$(vmrun -vp "${TPM_PASS}" getGuestIPAddress $p nogui)
+  else
+    address=$(vmrun getGuestIPAddress $p nogui)
+  fi
+  if (( $? != 0 )); then
+    echo ; echo "(PGM) vmrun finished with ERRORS !!!!!!"; echo
+    HEALTHCHECK_STATUS=BAD
+  fi
+  if [[ "${address}" =~ "Error" ]];then
+    echo cos nie tak
+    HEALTHCHECK_STATUS=BAD
+  else
+    echo "IP Address = $address (PGM)"
+  fi
+}
+
+#########################################################################################################
+spr_vmware_tools() {
+  if [ ! -z "${TPM_PASS:-}" ];then
+    status=$(vmrun -vp "${TPM_PASS}" checkToolsState $p nogui)
+  else
+    status=$(vmrun checkToolsState $p nogui)
+  fi
+  if (( $? != 0 )); then
+    echo ; echo "(PGM) vmrun finished with ERRORS !!!!!!"; echo
+    HEALTHCHECK_STATUS=BAD
+  fi
+
+  if [ "${status}" != "running" ];then
+    echo cos nie tak
+    HEALTHCHECK_STATUS=BAD
+  else
+    echo "vmare Tools are running (OK) (PGM)"
+  fi
+}
+#########################################################################################################
+#########################################################################################################
+#########################################################################################################
+
 export DISPLAY=
 
 echo vmrun list | boxes -s 40x5 -a c
@@ -23,19 +76,16 @@ export IFS=$'\n'
 
 for p in `vmrun list|grep vmx`;do
   echo
-  echo "* * * suspending $p (PGM) * * *"
-  vmrun getGuestIPAddress  $p nogui
-  if (( $? != 0 )); then
-    echo ; echo "(PGM) vmrun finished with ERRORS !!!!!!"; echo
-  fi
-  sleep 0.5 ;
+  echo "* * * checking $p (PGM) * * *"
+  spr_ip_address   $p
+  spr_vmware_tools $p
+  sleep 0.2 ;
 done;
 
-echo ; 
-
-echo vmrun list | boxes -s 40x5 -a c
-echo 
-vmrun list
-echo 
+if [ "${HEALTHCHECK_STATUS}" = "BAD" ];then
+  /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
+else
+  /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
+fi
 
 . /root/bin/_script_footer.sh
