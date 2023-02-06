4d15bc8c874c55950177a1a10a5225af59afc2c9
root <root@nuci7b.eth.che.matuszyk.com>
2023-02-06 15:57:31 +0100

    "new push from nuci7b.eth.che.matuszyk.com @ 2023.02.06 15:57:31"

---
+++ syslog-tail.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 2023.02.06 - v. 0.1 - initial release
+
+echo ; cat  $0|grep -e '# *20[123][0-9]'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}' ; echo ; echo
+
+figlet -w 120 "syslog tail -F"
+sleep 1.5s
+
+tail -F -n 2000 /var/log/syslog
+
