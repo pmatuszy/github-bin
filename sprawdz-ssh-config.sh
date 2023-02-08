7f89d27df27ea8bc34134687a83a362e58fb3254
root <pmatuszy.login+github@matuszyk.com>
2023-02-08 10:57:50 +0100

    "new push from zurich.eth.che.matuszyk.com @ 2023.02.08 10:57:50"

--- sprawdz-ssh-config.sh
+++ sprawdz-ssh-config.sh
@@ -7,9 +7,14 @@ if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
 fi
 
-# spr. czy dziala vpn
+type -fP keychain 2>&1 > /dev/null
+if (( $? != 0 )); then
+  echo ; echo "(PGM) I can't find keychain utility... exiting ..."; echo
+  exit 1
+fi
+
+# spr. config ssh
 if [ `ps -ef|grep vpnserver | awk '{print $8}'|grep -v grep|uniq|wc -l` -eq 0 ];then 
-#  (echo "vpn na `hostname` NIE dziala" | mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`/bin/hostname`-`date '+\%Y.\%m.\%d \%H:\%M:\%S'`) vpn nie dziala" matuszyk+`/bin/hostname`@matuszyk.com)
   /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
 else
   /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
@@ -19,11 +24,5 @@ exit
 #####
 # new crontab entry
 
-*/3 * * * * /root/bin/sprawdz-czy-dziala-server-vpn.sh
-
-# old crontab entry
-# spr. czy dziala vpn
-#5 */6 * * * if [ `ps -ef|grep vpnserver | awk '{print $8}'|grep -v grep|uniq|wc -l` -eq 0 ];then (echo "vpn na `hostname` NIE dziala" | mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`/bin/hostname`-`date '+\%Y.\%m.\%d \%H:\%M:\%S'`) vpn nie dziala" matuszyk+`/bin/hostname`@matuszyk.com) ; fi
-
-~
+*/3 * * * * /root/bin/sprawdz-ssh-config.sh
 
