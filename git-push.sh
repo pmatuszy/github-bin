4f0adf93be414b7a299d7bca095bb8956f7a9f15
root <pmatuszy.login+github@matuszyk.com>
2023-02-08 11:22:14 +0100

    "new push from zurich.eth.che.matuszyk.com @ 2023.02.08 11:22:14"

--- git-push.sh
+++ git-push.sh
@@ -32,7 +32,9 @@ if [ ! -d "${GIT_REPO_DIRECTORY}" ];then
 fi
 
 check_if_installed keychain
-eval $(keychain -q --eval id_ed25519 id_SSH_ed25519_20230207_OpenSSH)
+#eval $(keychain -q --eval id_ed25519 id_SSH_ed25519_20230207_OpenSSH)
+
+keychain --nocolor id_ed25519 id_SSH_ed25519_20230207_OpenSSH
 
 batch_mode=0
 
