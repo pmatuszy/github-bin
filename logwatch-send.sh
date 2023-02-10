c417a29b7207c128e3ec8fd62bac75bfab167f88
root <pmatuszy.login+github@matuszyk.com>
2023-02-10 09:29:19 +0100

    "new push from bitcoinnetworknode.eth.che.matuszyk.com @ 2023.02.10 09:29:19"

--- logwatch-send.sh
+++ logwatch-send.sh
@@ -7,6 +7,9 @@
 mail_subject="(`/bin/hostname`-`date '+%Y.%m.%d %H:%M:%S'`) logwatch"
 mail_recipient=matuszyk+`/bin/hostname`@matuszyk.com
 details_level=${1:-low}
+range="between -7 days and today"
+
+range="${2:-yesterday and today}"
 
 {
 
@@ -14,7 +17,7 @@ check_if_installed mailx
 check_if_installed strings
 check_if_installed aha
 
-/usr/sbin/logwatch --detail "${details_level}" 
+/usr/sbin/logwatch --detail "${details_level}" range="${range}"
 } | strings | aha | mailx -a 'Content-Type: text/html' -s "${mail_subject}"  ${mail_recipient}
 
 . /root/bin/_script_footer.sh
@@ -24,3 +27,4 @@ exit
 # new crontab entry
 
 0 5 * * *   /root/bin/logwatch-send.sh      # optional $1 level can be a positive integer, or high, med, low, which correspond to the integers 10, 5, and 0, respectively.
+                                            # optional $2 range e.g. "between -7 days and today" or "yesterday and today"
