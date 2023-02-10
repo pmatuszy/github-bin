ca65571b03cc6dc8d5b5ac8ec359b0e78dcf93fe
root <pmatuszy.login+github@matuszyk.com>
2023-02-10 09:01:12 +0100

    "new push from cloud.eth.r.matuszyk.com @ 2023.02.10 09:01:12"

---
+++ logwatch-send.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 2023.02.10 - v. 0.1 - initial release
+
+. /root/bin/_script_header.sh
+
+mail_subject="(`/bin/hostname`-`date '+%Y.%m.%d %H:%M:%S'`) logwatch"
+mail_recipient=matuszyk+`/bin/hostname`@matuszyk.com
+details_level=${1:-low}
+
+check_if_installed mailx
+check_if_installed strings
+check_if_installed aha
+
+/usr/sbin/logwatch --detail "${details_level}" | strings | aha | mailx -a 'Content-Type: text/html' -s "${mail_subject}"  ${mail_recipient}
+
+. /root/bin/_script_footer.sh
+
+exit
+#####
+# new crontab entry
+
+0 5 * * *   /root/bin/logwatch-send.sh      # optional $1 level can be a positive integer, or high, med, low, which correspond to the integers 10, 5, and 0, respectively.
--- sprawdz-ile-apt-list--upgradable.sh
+++ sprawdz-ile-apt-list--upgradable.sh
@@ -3,20 +3,12 @@
 # 2023.01.03 - v. 0.4 - dodano random delay jesli skrypt jest wywolywany nieinteraktywnie
 # 2022.06.15 - v. 0.3 - dodanie czekania jesli apt-get update jest wykonywany w tym samym czasie przez inny proces
 # 2022.05.05 - v. 0.2 - dodany uptime i hostname
-# 2022.05.03 - v. 0.1 - initial release (date unknown)
+# 20xx.xx.xx - v. 0.1 - initial release (date unknown)
 
 . /root/bin/_script_header.sh
+
 if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
-fi
-
-export MAX_RANDOM_DELAY_IN_SEC=${MAX_RANDOM_DELAY_IN_SEC:-50}
-tty 2>&1 >/dev/null
-if (( $? != 0 )); then      # we set RANDOM_DELAY only when running NOT from terminal
-  export RANDOM_DELAY=$((RANDOM % $MAX_RANDOM_DELAY_IN_SEC ))
-  sleep $RANDOM_DELAY
-else
-  echo ; echo "Interactive session detected: I will NOT introduce RANDOM_DELAY..."
 fi
 
 ile_max_by_nie_raportowac=45
@@ -47,6 +39,8 @@ if [ $ile_jest_do_upgradeowania -gt $ile_max_by_nie_raportowac ];then
 else
   /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 --data-raw "$m" -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
 fi
+
+/root/bin/_script_footer.sh
 
 exit
 #####
