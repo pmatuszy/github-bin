49b4bd471ffa5873ecfd55a0cce53c2dbb00f83b
root <pmatuszy.login+github@matuszyk.com>
2023-02-10 09:02:30 +0100

    "new push from cloud.eth.r.matuszyk.com @ 2023.02.10 09:02:30"

--- logwatch-send.sh
+++ logwatch-send.sh
@@ -8,11 +8,14 @@ mail_subject="(`/bin/hostname`-`date '+%Y.%m.%d %H:%M:%S'`) logwatch"
 mail_recipient=matuszyk+`/bin/hostname`@matuszyk.com
 details_level=${1:-low}
 
+{
+
 check_if_installed mailx
 check_if_installed strings
 check_if_installed aha
 
-/usr/sbin/logwatch --detail "${details_level}" | strings | aha | mailx -a 'Content-Type: text/html' -s "${mail_subject}"  ${mail_recipient}
+/usr/sbin/logwatch --detail "${details_level}" 
+} | strings | aha | mailx -a 'Content-Type: text/html' -s "${mail_subject}"  ${mail_recipient}
 
 . /root/bin/_script_footer.sh
 
