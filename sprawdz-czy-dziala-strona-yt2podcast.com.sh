f4934426a721f26700c38e6628a75939a1eb7a8a
root <pmatuszy.login+github@matuszyk.com>
2023-02-15 10:18:30 +0100

    "new push from backupche.eth.che.matuszyk.com @ 2023.02.15 10:18:30"

--- sprawdz-czy-dziala-strona-yt2podcast.com.sh
+++ sprawdz-czy-dziala-strona-yt2podcast.com.sh
@@ -12,10 +12,8 @@ if [ -f "$HEALTHCHECKS_FILE" ];then
 fi
 
 if [ `wget yt2podcast.com:8080 -qO - |grep .xml|wc -l` -gt 0 ];then 
-   # echo dziala strona yt2podcast.com:8080 | strings | aha | /usr/bin/mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`hostname --short`) OK - strona yt2podcast.com:8080 dziala" matuszyk+`hostname`@matuszyk.com
    /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
 else 
-   # echo "!!! NIE dziala strona yt2podcast.com:8080" | strings | aha | /usr/bin/mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`hostname --short`) PROBLEM - strona yt2podcast.com:8080 NIE dziala" matuszyk+`hostname`@matuszyk.com
    /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
 fi
 
