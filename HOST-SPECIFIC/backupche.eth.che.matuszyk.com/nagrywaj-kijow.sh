79f664e77e555685b9d41af14f263f57df42de67
root <pmatuszy.login+github@matuszyk.com>
2023-02-17 09:06:36 +0100

    "new push from backupche.eth.che.matuszyk.com @ 2023.02.17 09:06:36"

--- HOST-SPECIFIC/backupche.eth.che.matuszyk.com/nagrywaj-kijow.sh
+++ HOST-SPECIFIC/backupche.eth.che.matuszyk.com/nagrywaj-kijow.sh
@@ -17,7 +17,7 @@ while : ; do
   nazwa_pliku="Kijow-livecam_$(date '+%Y%m%d_%H%M%S').mp4"
   "${yt_bin}" --ignore-errors --no-part --output "${DOKAD_PREFIX}/${nazwa_pliku}" "$url" 
   chown "${wlasciciel_pliku}" "${DOKAD_PREFIX}/${nazwa_pliku}" 2>/dev/null
-  (echo "koniec wykonywania $0" && ls -lr "${DOKAD_PREFIX}") | strings | aha | \
-      mailx -r root@`hostname` -a 'Content-Type: text/html' -s "$0 (`/bin/hostname`-`date '+%Y.%m.%d %H:%M:%S'`)" matuszyk@matuszyk.com
+#  (echo "koniec wykonywania $0" && ls -lr "${DOKAD_PREFIX}") | strings | aha | \
+#      mailx -r root@`hostname` -a 'Content-Type: text/html' -s "$0 (`/bin/hostname`-`date '+%Y.%m.%d %H:%M:%S'`)" matuszyk@matuszyk.com
   sleep ${opoznienie_miedzy_wywolaniami}
 done
