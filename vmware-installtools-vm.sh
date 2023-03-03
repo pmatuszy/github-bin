8f7ecbc768fb21972b721a082ad2d7d8f4b555e0
root <pmatuszy.login+github@matuszyk.com>
2023-03-03 14:18:05 +0100

    "new push from bitcoinnetworknode.eth.che.matuszyk.com @ 2023.03.03 14:18:05"

--- vmware-installtools-vm.sh
+++ vmware-installtools-vm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2023.03.03 - v. 0.3 - if open-vm-tools is installed we will uninstall it
 # 2023.03.03 - v. 0.2 - if cdrom is already mounted under /mnt/tmp we do not remount it again....
 # 2023.02.17 - v. 0.1 - initial release
 
@@ -9,6 +10,15 @@ if [ ! -f  /mnt/tmp/vmware-tools-upgrader-64 ];then
   mkdir -p /mnt/tmp
   mount /dev/cdrom /mnt/tmp || exit 2
 fi
+
+# if [ !  $(dpkg -s open-vm-toolsdd >/dev/null 2>&1 ) ];then    # openvm tools are NOT installed
+if [ ! $(dpkg -s open-vm-tools >/dev/null 2>&1 ) ];then    # openvm tools are NOT installed
+  echo "usuwam open-vm-tools, bo nie chce uzywac tego pakietu" | boxes -s 50x3 -a c -d ada-box
+  echo apt remove -y open-vm-tools | boxes -s 50x3 -a c -d ada-box
+  # apt remove -y open-vm-tools
+fi
+
+exit
 
 {
 rm /tmp/VMwareTools-* 2>/dev/null
