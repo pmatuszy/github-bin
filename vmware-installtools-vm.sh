787b7ad0bc4bda877d388e11f98ac36a02142896
root <pmatuszy.login+github@matuszyk.com>
2023-03-03 16:47:30 +0100

    "new push from bitcoinnetworknode.eth.che.matuszyk.com @ 2023.03.03 16:47:30"

--- vmware-installtools-vm.sh
+++ vmware-installtools-vm.sh
@@ -11,7 +11,7 @@ if [ ! -f  /mnt/tmp/vmware-tools-upgrader-64 ];then
   mount /dev/cdrom /mnt/tmp || exit 2
 fi
 
-if [ $(dpkg -s open-vm-tools >/dev/null 2>&1 ) ];then    # openvm tools are NOT installed
+if [ ! $(dpkg -s open-vm-tools >/dev/null 2>&1 ) ];then    # openvm tools are NOT installed
   echo "usuwam open-vm-tools, bo nie chce uzywac tego pakietu" | boxes -s 50x3 -a c -d ada-box
   echo apt remove -y open-vm-tools | boxes -s 50x3 -a c -d ada-box
   apt remove -y --purge open-vm-tools >/dev/null 2>&1
