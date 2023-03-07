6cd489df32242e59b24fc0c88d9596177f64f5b5
root <pmatuszy.login+github@matuszyk.com>
2023-03-07 22:28:47 +0100

    "new push from nucvm.eth.che.matuszyk.com @ 2023.03.07 22:28:47"

--- jd.sh
+++ jd.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# 2023.03.07 - v. 0.8 - added script_header and footer calls
 # 2022.11.10 - v. 0.7 - added S/N printing for NVME devices and replaced echo with printf to beautify output
 # 2022.10.27 - v. 0.6 - bugfixes, added support for Raspbian, small changes in print format output,added printing of the script version
 # 2022.10.11 - v. 0.5 - discovery if it is x86 or raspberry pi
@@ -6,6 +8,8 @@
 # 2020.11.27 - v. 0.3 - added printing of the disk serial numbers
 # 2020.10.09 - v. 0.2 - small cosmetic modifications
 # 2020.0x.xx - v. 0.1 - initial release (date unknown)
+
+. /root/bin/_script_header.sh
 
 if [[ ! -h /etc/os-release && ! -h /etc/redhat-release ]] ; then
   echo
@@ -15,8 +19,6 @@ if [[ ! -h /etc/os-release && ! -h /etc/redhat-release ]] ; then
 fi
 
 echo 
-
-cat  $0|grep -e '# *20[123][0-9]'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}' ; echo ; echo
 
 if [[ `cat /etc/os-release /etc/redhat-release 2>/dev/null |grep -qi centos ; echo $?` == 0 ]] ; then
   echo CENTOS | boxes -s 40x5 -a c ; echo 
@@ -35,14 +37,11 @@ echo $hardware_type | boxes -s 40x5 -a c ; echo
 if [[ `cat /etc/os-release /etc/redhat-release 2>/dev/null |egrep -qi "ubuntu|Raspbian" ; echo $?` == 0 ]] ; then
   for p in `fdisk -l|grep 'Disk /dev'|egrep -v 'mapper|/md|/ram|mmcb|/dev/loop'|awk '{print $2}'|sort|tr -d :`;do
      if [[ `hdparm -I $p 2>/dev/null | grep 'Serial\ Number'|wc -l` == 0 ]]; then   # no S/N - I assume it is NVME disk...
-#       echo `gdisk -l $p |grep "Disk /dev"` "     Serial Number: " $(nvme list | grep $p | awk '{print $2}')
        printf "%-45s" "`gdisk -l $p |grep "Disk /dev"`"
        printf "  Serial Number: %-20s\n" "$(nvme list | grep $p | awk '{print $2}')"
-      
      else
        printf "%-45s" "`gdisk -l $p |grep "Disk /dev"`"
        printf "%-45s\n" "$(hdparm -I $p 2>/dev/null | grep 'Serial\ Number')" | sed 's|  *| |g'
-#        echo `gdisk -l $p |grep "Disk /dev"` "   " `hdparm -I $p 2>/dev/null | grep 'Serial\ Number'` ;
      fi
   done
 
@@ -51,3 +50,6 @@ if [[ `cat /etc/os-release /etc/redhat-release 2>/dev/null |egrep -qi "ubuntu|Ra
 fi
 
 echo
+
+. /root/bin/_script_footer.sh
+
