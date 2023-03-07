57df4a268b40c0d071d0faf2e8d94f7260cb8d59
root <pmatuszy.login+github@matuszyk.com>
2023-03-07 11:18:35 +0100

    "new push from nucvm.eth.che.matuszyk.com @ 2023.03.07 11:18:35"

--- wakeonlan-pgm-che.sh
+++ wakeonlan-pgm-che.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# 2023.03.07 - v. 0.2 - added check for wakeonlan package
 # 2023.01.29 - v. 0.1 - initial release
 
 . /root/bin/_script_header.sh
+
+check_if_installed wakeonlan
 
 opoznienie=15
 
