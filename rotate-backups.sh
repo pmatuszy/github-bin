0234f8c00c1589aa84a6f239fc47dbc0feabe2a4
root <pmatuszy.login+github@matuszyk.com>
2023-03-13 13:57:14 +0100

    "new push from cloud.eth.r.matuszyk.com @ 2023.03.13 13:57:14"

--- rotate-backups.sh
+++ rotate-backups.sh
@@ -9,6 +9,7 @@ if [ -f "$HEALTHCHECKS_FILE" ];then
 fi
 
 check_if_installed pip python3-pip
+check_if_installed curl
 
 if (( $# < 2 )) ; then
   echo ; echo "(PGM) wrong # of command line arguments... (must be more than 1)" ; echo 
@@ -27,15 +28,29 @@ if [ ! $? ] ; then     # if binary can't be found we do not continue
   exit 3
 fi
 
-echo $CMD $*
-HC_MESSAGE=$($CMD $*  2>&1 |sed 's|^.* INFO ||g' ; exit $?)
+HC_MESSAGE=$(
+   cat  $0|grep -e '# *20[123][0-9]'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}'
+   echo ; echo "aktualna data: `date '+%Y.%m.%d %H:%M'`" ; echo ;
+   echo $* | grep -qi -- "--dry-run"
+   if (( $? == 0 ));then
+     echo "dry run: ON"  | boxes -s 50x3 -a c -d ada-box
+   else
+     echo "dry run: OFF" | boxes -s 50x3 -a c -d ada-box
+   fi
+   echo ; echo "command to be run:"
+   echo $CMD $*  2>&1 |sed 's|^.* INFO ||g' ; echo 
+        $CMD $*  2>&1 |sed 's|^.* INFO ||g' 
+   exit $?
+   )
 
 echo "$HC_MESSAGE" | /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 --data-binary @- -o /dev/null "$HEALTHCHECK_URL"/$? 2>/dev/null
 
 exit $?
+
 #####
 # new crontab entry
 
-@reboot ( sleep 3m && /root/bin/rotate-backups.sh )
+@reboot ( sleep 3m && /root/bin/rotate-backups.sh  --dry-run --syslog=no --relaxed --hourly=24*2 --daily=10 /xxxx/yyyy )
+1 0 * * *    /root/bin/rotate-backups.sh --dry-run --syslog=no --relaxed --hourly=24*2 --daily=10 /xxxx/yyyy
+rotate-backups --dry-run --relaxed --hourly="20*24" --daily=366 --weekly=56 --monthly=24 --yearly=always --ionice=idle /xxxx/yyyy
 
-1 0 * * *    /root/bin/rotate-backups.sh --dry-run --syslog=no --relaxed --hourly=24*2 XXXXXXXXXXXX/XXXXXXXXXX
