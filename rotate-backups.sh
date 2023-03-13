af8466e55cc0a8c5b17933da5ca162b75c726b60
root <pmatuszy.login+github@matuszyk.com>
2023-03-13 07:03:22 +0100

    "new push from cloud.eth.r.matuszyk.com @ 2023.03.13 07:03:22"

---
+++ rotate-backups.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# 2023.03.12 - v. 0.1 - initial release
+
+. /root/bin/_script_header.sh
+
+if [ -f "$HEALTHCHECKS_FILE" ];then
+  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
+export warnings_and_errors=0
+
+
+check_if_installed pip python3-pip
+
+if (( $# != 2 )) ; then
+  echo ; echo "(PGM) wrong # of command line arguments... (must be precisely 2 but you provided $#)" ; echo 
+  exit 1
+fi
+
+exit
+
+
+HC_message=$(
+  warnings_and_errors=0
+
+  cat  $0|grep -e '# *20[123][0-9]'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}'
+  echo "aktualna data: `date '+%Y.%m.%d %H:%M'`" ; echo ;
+
+  keychain  --nocolor id_rsa id_ed25519 id_SSH_ed25519_20230207_OpenSSH 2>&1 | egrep -iq "warning|error"
+  
+  if (( $? == 0 )); then               # exit status = 0 oznacza, ze linie ZNALEZIONO, wiec jest blad
+    let warnings_and_errors=warnings_and_errors+1
+    keychain --nogui --nocolor id_rsa id_ed25519 id_SSH_ed25519_20230207_OpenSSH 2>&1 | egrep -i "warning|error"
+    echo "(PGM) NOT all 3 keys are loaded - PROBLEM " | boxes -s 50x3 -a c -d ada-box
+    echo ;  echo
+  else
+    echo "(PGM) 3 keys are loaded - looks GOOD" | boxes -s 50x3 -a c -d ada-box
+  fi
+  
+  echo
+  echo "keychain --nogui --nocolor id_rsa id_ed25519 id_SSH_ed25519_20230207_OpenSSH"
+  echo
+        keychain --nogui --nocolor id_rsa id_ed25519 id_SSH_ed25519_20230207_OpenSSH 2>&1
+  
+  how_many=$(keychain --nogui --nocolor id_rsa id_ed25519 id_SSH_ed25519_20230207_OpenSSH 2>&1 | \
+             egrep -i "Known ssh key: .*/id_rsa|Known ssh key: .*/id_SSH_ed25519_20230207_OpenSSH|Known ssh key: .*/id_ed25519" | wc -l)
+  
+  if (( $how_many != 3 )); then
+    let warnings_and_errors=warnings_and_errors+1
+    echo "(PGM) NOT all 3 keys are known - PROBLEM" | boxes -s 50x3 -a c -d ada-box
+  else
+    echo "(PGM) all 3 keys are known - looks GOOD" | boxes -s 50x3 -a c -d ada-box
+  fi
+  
+  echo ; echo
+  sleep 1
+  echo keychain --nogui --nocolor -l | boxes -s 50x3 -a c -d ada-box
+       keychain --nogui --nocolor -l 2>&1
+  echo
+  exit $warnings_and_errors
+)
+
+kod_powrotu=$?
+
+if (( script_is_run_interactively )) ;then
+  echo "$HC_message"
+fi
+
+echo "$HC_message" | /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 --data-binary @- -o /dev/null "$HEALTHCHECK_URL"/${kod_powrotu} 2>/dev/null
+
+. /root/bin/_script_footer.sh
+
+exit ${kod_powrotu}
+
+#####
+# new crontab entry
+
+@reboot ( sleep 55 && /root/bin/ssh-keychain.sh )
+
+1 0 * * *    /root/bin/rotate-backups.sh 
