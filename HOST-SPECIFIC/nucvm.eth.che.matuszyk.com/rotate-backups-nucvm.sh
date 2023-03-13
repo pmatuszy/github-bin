f9868cc18dda2606cd6d8eabee3c73aa5c31bc8c
root <pmatuszy.login+github@matuszyk.com>
2023-03-13 15:56:25 +0100

    "new push from nucvm.eth.che.matuszyk.com @ 2023.03.13 15:56:25"

--- HOST-SPECIFIC/nucvm.eth.che.matuszyk.com/rotate-backups-nucvm.sh
+++ HOST-SPECIFIC/nucvm.eth.che.matuszyk.com/rotate-backups-nucvm.sh
@@ -6,8 +6,8 @@ policy='--dry-run --relaxed --hourly="20*24" --daily=366 --weekly=56 --monthly=2
 
 export HEALTHCHECKS_FORCE_ID=rotate-backups.sh-www02 
 export katalog=/mnt/luks-buffalo2/_backupy-1dyne_kopie/www02
-eval /root/bin/rotate-backups.sh $policy $katalog | sed "s|${katalog}/||g"
+eval /root/bin/rotate-backups.sh $policy $katalog
 
 export HEALTHCHECKS_FORCE_ID=rotate-backups.sh-cloud
 export katalog=/mnt/luks-buffalo2/_backupy-1dyne_kopie/cloud-var-www
-eval /root/bin/rotate-backups.sh $policy $katalog | sed "s|${katalog}/||g"
+eval /root/bin/rotate-backups.sh $policy $katalog
