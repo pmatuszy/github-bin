47caaa39de04d6d6a233d173621580b02dc50398
root <pmatuszy.login+github@matuszyk.com>
2023-03-13 16:25:13 +0100

    "new push from nucvm.eth.che.matuszyk.com @ 2023.03.13 16:25:13"

---
+++ HOST-SPECIFIC/nucvm.eth.che.matuszyk.com/sciagnij-backupy-nucvm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 2023.03.13 - v. 0.1 - initial release
+
+export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-www02
+/root/bin/sciagnij-backupy.sh www02.eth.r.matuszyk.com:/var/www/202*bz2 /mnt/luks-buffalo2/_backupy-1dyne_kopie/www02
+
+
---
+++ sciagnij-backupy.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# 2023.03.13 - v. 0.1 - initial release
+
+. /root/bin/_script_header.sh
+
+if [ -f "$HEALTHCHECKS_FILE" ];then
+  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
+fi
+
+if [ ! -z "${HEALTHCHECKS_FORCE_ID:-}" ]; then
+  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^$HEALTHCHECKS_FORCE_ID"|awk '{print $2}')
+fi
+
+check_if_installed curl
+check_if_installed rsync
+
+if (( $# != 2 )) ; then
+  echo ; echo "(PGM) wrong # of command line arguments... (must be exactly 2)" ; echo 
+  exit 1
+fi
+
+if [ ! -d "${@:$#}" ];then
+  echo ; echo "(PGM) Directory $2 doesn't exist..." ; echo
+  exit 2
+fi
+
+export SKAD=$1
+export DOKAD="$2"
+export rsync_option="-a -v --stats --bwlimit=990000 --no-compress --progress --info=progress1 --partial  --inplace --remove-source-files"
+export rsync_options="-a -v --stats --bwlimit=990000 --no-compress --progress --info=progress1 --partial  --inplace "
+
+HC_MESSAGE=$(
+   cat  $0|grep -e '# *20[123][0-9]'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}'
+   echo ; echo "aktualna data: `date '+%Y.%m.%d %H:%M'`" ; echo ;
+   
+   echo ; echo  ; echo "SKAD  = $SKAD" ; echo "DOKAD = $DOKAD" ; echo 
+   eval rsync $rsync_options ${SKAD} "${DOKAD}"
+
+   echo ; echo "command to be run:"
+   echo $CMD $*  2>&1 |sed 's|^.* INFO ||g' ; echo 
+        $CMD $*  2>&1 |sed 's|^.* INFO ||g' | sed "s|"${@:$#}"/||g" 
+   exit $?
+   )
+
+kod_powrotu=$?
+
+if (( $script_is_run_interactively == 1 )); then
+  echo "$HC_MESSAGE"
+fi
+
+echo "$HC_MESSAGE" | /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 --data-binary @- -o /dev/null "$HEALTHCHECK_URL"/$kod_powrotu 2>/dev/null
+
+exit $?
+
+#####
+# new crontab entry
+
