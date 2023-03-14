99f4c3e7463aea58ae628fa32641aead83175e05
root <pmatuszy.login+github@matuszyk.com>
2023-03-14 10:27:22 +0100

    "new push from nucvm.eth.che.matuszyk.com @ 2023.03.14 10:27:22"

--- HOST-SPECIFIC/nucvm.eth.che.matuszyk.com/sciagnij-backupy-nucvm.sh
+++ HOST-SPECIFIC/nucvm.eth.che.matuszyk.com/sciagnij-backupy-nucvm.sh
@@ -9,7 +9,7 @@ DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/www02
 eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD
 
 
-export HEALTHCHECKS_FORCE_ID=rotate-backups.sh-cloud
+export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-cloud
 SKAD=cloud.eth.r.matuszyk.com:/var/www/202*bz2
 DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/cloud-var-www
 
