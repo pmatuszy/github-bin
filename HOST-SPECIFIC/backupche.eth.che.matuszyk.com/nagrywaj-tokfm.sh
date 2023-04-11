cde780037391390c50a27546a72c8ac0b85b33be
root <pmatuszy.login+github@matuszyk.com>
2023-04-11 18:39:42 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2023.04.11 18:39:42"

--- HOST-SPECIFIC/backupche.eth.che.matuszyk.com/nagrywaj-tokfm.sh
+++ HOST-SPECIFIC/backupche.eth.che.matuszyk.com/nagrywaj-tokfm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# 2023.04.11 - v. 0.8 - almost new version of the script (like BBC one)
 # 2022.12.14 - v. 0.7 - dodalem zmiena opoznienie_miedzy_wywolaniami zamiast hardcoded 10s
 # 2022.05.16 - v. 0.6 - chown redirection to dev null
 # 2022.03.11 - v. 0.7 - bug fix DOKAD ==> DOKAD_PREFIX
@@ -9,35 +11,30 @@
 # 2022.01.26 - v. 0.2 - jak ffmpeg sie skonczy wczesniej to restartujemy nagrywanie do polnocy + 1 minuta
 # 2022.01.13 - v. 0.1 - initial release (date unknown)
 
+. /root/bin/_script_header.sh
+
 # SKAD="http://gdansk1-1.radio.pionier.net.pl:8000/pl/tuba10-1.mp3"
 export SKAD="http://poznan5-4.radio.pionier.net.pl:8000/tuba10-1.mp3"
 export DOKAD_PREFIX="/worek-samba/nagrania/TokFM-nagrania/tokFM"
 
 wlasciciel_pliku="che:che"
-opoznienie_miedzy_wywolaniami=20s
+opoznienie_miedzy_wywolaniami=40s
 
-czas_startu_skryptu=`date '+%s'`
+secs_to_midnight=$((($(date -d "tomorrow 00:00" +%s)-$(date +%s))))
 
-secs_to_midnight=$((($(date -d "tomorrow 00:00" +%s)-$czas_startu_skryptu)))
-let max_timestamp_dzialania_skryptu=$((($(date +%s)+$secs_to_midnight+20)))
-
-# echo "czas_startu_skryptu        = $czas_startu_skryptu (`date -d @$czas_startu_skryptu`), max_timestamp_dzialania_skryptu = $max_timestamp_dzialania_skryptu (`date -d @$max_timestamp_dzialania_skryptu`), aktualny czas: $(date +%s) (`date`)"
-
-while (( $(date +%s) - $max_timestamp_dzialania_skryptu <= 0 )) ; do      # spr czy akt sekunda jest mniejsza niz max sekunda, kiedy moze dzialas skrypt
-  (echo "POCZATEK wykonywania $0" && ls -lr `dirname "${DOKAD_PREFIX}"`) | strings | aha | \
-      mailx -r root@`hostname` -a 'Content-Type: text/html' -s "$0 (`/bin/hostname`-`date '+%Y.%m.%d %H:%M:%S'`)" matuszyk@matuszyk.com
+while (( $secs_to_midnight > 200 )) ; do
+  secs_to_midnight=$((($(date -d "tomorrow 00:00" +%s)-$(date +%s))))
   let secs_nagrywania=secs_to_midnight+60
   DOKAD="${DOKAD_PREFIX}-`date '+%Y.%m.%d__%H%M%S'`.mp3"
-  timeout --preserve-status --signal=HUP --kill-after=$((secs_nagrywania+120)) $((secs_nagrywania+60)) ffmpeg -hide_banner -loglevel quiet -t "${secs_nagrywania}" -i "$SKAD" "$DOKAD"
 
-  chown "${wlasciciel_pliku}" "${DOKAD}" 2>/dev/null
-  sleep $opoznienie_miedzy_wywolaniami # opozniamy bo jak sa problemy z siecia, to by nie startowac od razu z nastepna proba...
-  secs_to_midnight=$((($(date -d "tomorrow 00:00" +%s)-$(date +%s))))
-
+  ffmpeg -hide_banner -loglevel quiet -t "${secs_nagrywania}" -i "$SKAD" "$DOKAD" 2>&1
+  kod_powrotu=$?
+  chown "${wlasciciel_pliku}" "${DOKAD}"
+  if (( $kod_powrotu == 0 ));then
+    break
+  fi
+  sleep ${opoznienie_miedzy_wywolaniami} # opozniamy bo jak sa problemy z siecia, to by nie startowac od razu z nastepna proba...
 done
 
-if [ -z ${STY:-} ]; then    # checking if we are running interactively
-  (echo "koniec wykonywania $0" && ls -lr `dirname "${DOKAD_PREFIX}"`) | strings | aha | \
-      mailx -r root@`hostname` -a 'Content-Type: text/html' -s "$0 (`/bin/hostname`-`date '+%Y.%m.%d %H:%M:%S'`)" matuszyk@matuszyk.com
-fi
+. /root/bin/_script_footer.sh
 
