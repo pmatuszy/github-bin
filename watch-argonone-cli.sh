c408f97959d2c02cf6ffc8d0f96007f0c4efee24
root <pmatuszy.login+github@matuszyk.com>
2023-04-13 17:03:42 +0200

    "new push from nucvm.eth.che.matuszyk.com @ 2023.04.13 17:03:42"

--- watch-argonone-cli.sh
+++ watch-argonone-cli.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# 2023.04.13 - v. 0.2 - added check if argonone-cli is installed
 # 2021.09.19 - v. 0.1 - inicjalna wersja skryptu
+
+type -fP argonone-cli 2>&1 > /dev/null
+if (( $? != 0 )); then
+  echo ; echo "(PGM) I can't find argonone-cli utility... exiting ..."; echo
+  exit 1
+fi
 
 watch -n 0.2 argonone-cli --decode
