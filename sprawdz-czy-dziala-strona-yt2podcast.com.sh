0f1858b55b70936fb4a46e8417fd77569e0b69f7
root <pmatuszy.login+github@matuszyk.com>
2023-04-13 17:14:13 +0200

    "new push from nucvm.eth.che.matuszyk.com @ 2023.04.13 17:14:13"

--- sprawdz-czy-dziala-strona-yt2podcast.com.sh
+++ sprawdz-czy-dziala-strona-yt2podcast.com.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2023.04.13 - v. 0.6 - added how_many_retries and retry_delay
 # 2023.01.17 - v. 0.5 - dodano random delay jesli skrypt jest wywolywany nieinteraktywnie
 # 2022.05.16 - v. 0.3 - commented out sending emails sections
 # 2022.05.03 - v. 0.2 - added healthcheck support
@@ -11,11 +12,21 @@ if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
 fi
 
-if [ `wget yt2podcast.com:8080 -qO - |grep .xml|wc -l` -gt 0 ];then 
-   /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
-else 
-   /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
-fi
+url=yt2podcast.com:8080
+blad=1
+how_many_retries=5
+retry_delay=5
+
+while (( $blad != 0 && $how_many_retries != 0 )) ; do
+  if [ `wget $url -qO - |grep .xml|wc -l` -gt 0 ];then 
+    /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/null
+    blad=0
+  else 
+    /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
+    let how_many_retries=how_many_retries-1
+    sleep $retry_delay
+  fi
+done
 
 . /root/bin/_script_footer.sh
 
