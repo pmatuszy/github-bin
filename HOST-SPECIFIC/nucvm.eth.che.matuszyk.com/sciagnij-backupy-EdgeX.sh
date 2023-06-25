2b3410f86469d1fa8f18559d12be4d4ab768d863
root <pmatuszy.login+github@matuszyk.com>
2023-06-25 16:48:47 +0200

    "new push from nucvm.eth.che.matuszyk.com @ 2023.06.25 16:48:47"

--- HOST-SPECIFIC/nucvm.eth.che.matuszyk.com/sciagnij-backupy-EdgeX.sh
+++ HOST-SPECIFIC/nucvm.eth.che.matuszyk.com/sciagnij-backupy-EdgeX.sh
@@ -2,15 +2,23 @@
 
 # 2023.03.14 - v. 0.1 - initial release
 
+
+
+-----------
+-- EdgeR --
+-----------
+
 export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-EdgeR
 
 SKAD=192.168.1.1:/root/config/*
 DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/UBNT/EdgeR/EdgeR-root_config
-
 eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD --remove-source-files
 
 SKAD=192.168.1.1:/root/adresy-ip-historia/*
 DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/UBNT/EdgeR/EdgeR-root-adresy-ip-historia
+eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD --remove-source-files
 
+SKAD=192.168.1.1:/config/ARCHIWUM_CONFIGOW-EdgeR/config.boot-Edge*
+DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/UBNT/EdgeR/ARCHIWUM_CONFIGOW-EdgeR
 eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD --remove-source-files
 
