41f9127a4d62a7cafcd0cf15261911205bcbc800
root <pmatuszy.login+github@matuszyk.com>
2023-06-25 18:05:00 +0200

    "new push from nucvm.eth.che.matuszyk.com @ 2023.06.25 18:05:00"

--- HOST-SPECIFIC/nucvm.eth.che.matuszyk.com/sciagnij-backupy-EdgeX.sh
+++ HOST-SPECIFIC/nucvm.eth.che.matuszyk.com/sciagnij-backupy-EdgeX.sh
@@ -7,7 +7,6 @@
 ##########
 
 export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-EdgeSS_kat1
-
 SKAD=192.168.17.1:/root/config/*
 DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/UBNT/EdgeSS/EdgeSS-root_config
 eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD --remove-source-files
@@ -26,16 +25,17 @@ eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD --remove-source-files
 # EdgeCHE #
 ###########
 
-export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-EdgeCHE
-
+export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-EdgeCHE_kat1
 SKAD=192.168.200.1:/root/config/*
 DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/UBNT/EdgeCHE/EdgeCHE-root_config
 eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD --remove-source-files
 
+export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-EdgeCHE_kat2
 SKAD=192.168.200.1:/root/adresy-ip-historia/*
 DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/UBNT/EdgeCHE/EdgeCHE-root-adresy-ip-historia
 eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD --remove-source-files
 
+export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-EdgeCHE_kat3
 SKAD=192.168.200.1:/config/ARCHIWUM_CONFIGOW-EdgeCHE/config.boot-Edge*
 DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/UBNT/EdgeCHE/EdgeCHE-root_config/ARCHIWUM_CONFIGOW-EdgeCHE
 eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD --remove-source-files
@@ -44,16 +44,17 @@ eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD --remove-source-files
 # EdgeR #
 #########
 
-export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-EdgeR
-
+export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-EdgeR_kat1
 SKAD=192.168.1.1:/root/config/*
 DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/UBNT/EdgeR/EdgeR-root_config
 eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD --remove-source-files
 
+export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-EdgeR_kat2
 SKAD=192.168.1.1:/root/adresy-ip-historia/*
 DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/UBNT/EdgeR/EdgeR-root-adresy-ip-historia
 eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD --remove-source-files
 
+export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-EdgeR_kat3
 SKAD=192.168.1.1:/config/ARCHIWUM_CONFIGOW-EdgeR/config.boot-Edge*
 DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/UBNT/EdgeR/EdgeR-root_config/ARCHIWUM_CONFIGOW-EdgeR
 eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD --remove-source-files
