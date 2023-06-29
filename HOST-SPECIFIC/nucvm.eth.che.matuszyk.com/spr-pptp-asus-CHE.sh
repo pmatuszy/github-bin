330415d3b61886e1f927ec23d8895d08eefd84db
root <pmatuszy.login+github@matuszyk.com>
2023-06-29 21:15:46 +0200

    "new push from nucvm.eth.che.matuszyk.com @ 2023.06.29 21:15:46"

--- HOST-SPECIFIC/nucvm.eth.che.matuszyk.com/spr-pptp-asus-CHE.sh
+++ HOST-SPECIFIC/nucvm.eth.che.matuszyk.com/spr-pptp-asus-CHE.sh
@@ -4,12 +4,11 @@
 
 . /root/bin/_script_header.sh
 
+
+export adres_publiczny_z_pl="31.179.173.42"
+
 if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
-fi
-
-if [ ! -z "${HEALTHCHECKS_FORCE_ID:-}" ]; then
-  HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^$HEALTHCHECKS_FORCE_ID"|awk '{print $2}')
 fi
 
 if [ -f $HOME/.keychain/$HOSTNAME-sh ];then
@@ -19,18 +18,41 @@ fi
 check_if_installed curl
 check_if_installed scp openssh-client
 
-RSYNC_BIN="$(type -Pf rsync)"
-
-
 HC_MESSAGE=$(
    cat  $0|grep -e '# *20[123][0-9]'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}'
    echo ; echo "aktualna data: `date '+%Y.%m.%d %H:%M'`" ; echo ;
 
-   echo ; echo "command to be run:"
+   echo ; echo "checking if ppp interface is up..."
    echo ssh  admin@192.168.200.230 "ifconfig -a" | sed -n '/^ppp[0-9]/,/^$/p'
-   ssh  admin@192.168.200.230 "ifconfig -a" | sed -n '/^ppp[0-9]/,/^$/p'
-   ssh admin@192.168.200.230 "ping -c 1 -W 2 -q 192.168.133.1 " | grep -vq ", 0 packets received"    # jesli znajdzie taka linie to kod powrotu bedzie <> 0
-   exit $?
+   timeout --preserve-status --kill-after=15 --signal=SIGKILL 10s ssh admin@192.168.200.230 "ifconfig -a" | sed -n '/^ppp[0-9]/,/^$/p'
+   exit_code_1=$?
+
+   if (( $exit_code_1 == 0 ));then
+     echo "GOOD"
+   else
+     echo "NOT good"
+   fi
+
+   echo ; echo -n "checking if 192.168.1.1 is pingable... "
+   timeout --preserve-status --kill-after=15 --signal=SIGKILL 10s ssh admin@192.168.200.230 "ping -c 1 -W 2 -q 192.168.1.1 " | grep -vq ", 0 packets received"    # jesli znajdzie taka linie to kod powrotu bedzie <> 0
+   exit_code_2=$?
+   if (( $exit_code_2 == 0 ));then
+     echo "GOOD"
+   else
+     echo "NOT good"
+   fi
+
+   echo ; echo -n "checking if our public IP address is from PL... "
+   timeout --preserve-status --kill-after=15 --signal=SIGKILL 10s ssh admin@192.168.200.230 "/usr/sbin/curl --silent ifconfig.me | grep -q ${adres_publiczny_z_pl}"
+   exit_code_3=$?
+   if (( $exit_code_3 == 0 ));then
+     echo "GOOD"
+   else
+     echo "NOT good"
+   fi
+
+   let final_exit_code=exit_code_1+exit_code_2+exit_code_3
+   exit $final_exit_code
    )
 kod_powrotu=$?
 
