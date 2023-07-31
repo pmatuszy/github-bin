1ca2e84ec67621d4107695d19cb16ea8b7e2d495
root <pmatuszy.login+github@matuszyk.com>
2023-07-31 15:21:36 +0200

    "new push from rpir2.eth.r.matuszyk.com @ 2023.07.31 15:21:36"

--- snap-pgm-remove-old-version.sh
+++ snap-pgm-remove-old-version.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# 2023.07.31 - v. 0.2 - added check for snap command (and if it is not there then install it)
 # 2023.07.31 - v. 0.1 - initial release
 
 . /root/bin/_script_header.sh
+
+check_if_installed snap
 
 # from https://askubuntu.com/questions/1371833/howto-free-up-space-properly-on-my-var-lib-snapd-filesystem-when-snapd-is-unava
 
