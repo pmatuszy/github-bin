509304492e071c9883a7886b6ee4477231efefa3
root <pmatuszy.login+github@matuszyk.com>
2023-07-31 15:26:09 +0200

    "new push from nucvm.eth.che.matuszyk.com @ 2023.07.31 15:26:09"

--- snap-pgm-remove-old-version.sh
+++ snap-pgm-remove-old-version.sh
@@ -6,8 +6,17 @@
 . /root/bin/_script_header.sh
 
 check_if_installed snap
+check_if_installed boxes
 
 # from https://askubuntu.com/questions/1371833/howto-free-up-space-properly-on-my-var-lib-snapd-filesystem-when-snapd-is-unava
+
+echo "(PGM) All snap releases:" | boxes -a c -d stone
+snap list --all
+
+echo
+echo "(PGM) Snap released disabled which will be removed:" | boxes -a c -d stone
+snap list --all | grep disabled
+echo 
 
 LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
 while read pkg revision; do
