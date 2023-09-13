cd16f3288d029ac627c1a75d38de0c4ddf651188
root <pmatuszy.login+github@matuszyk.com>
2023-09-13 08:19:29 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2023.09.13 08:19:29"

--- 32or64.sh
+++ 32or64.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# 2023.09.13 - v. 0.2 - added invocation of script_header and script_footer
 # 2022.12.02 - v. 0.1 - inicjalna wersja skryptu
 
-cat  $0|grep -e '# *20[123][0-9]'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}' ; echo
+. /root/bin/_script_header.sh
 
 echo "Your system is $(getconf LONG_BIT)-bit" ; echo
 
+. /root/bin/_script_footer.sh
