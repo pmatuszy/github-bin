98e97f85661feac3946806144843cda6ae62e51a
root <pmatuszy.login+github@matuszyk.com>
2023-10-02 10:33:12 +0200

    "new push from nucvm.eth.che.matuszyk.com @ 2023.10.02 10:33:12"

--- wakeonlan-pgm-che.sh
+++ wakeonlan-pgm-che.sh
@@ -9,10 +9,12 @@ check_if_installed wakeonlan
 
 opoznienie=15
 
-for p in {1..8};do 
+for p in {1..9};do 
   wakeonlan -i 192.168.200.255 04:D9:F5:60:42:4A
   echo opoznienie $opoznienie
   sleep $opoznienie
 done
+
+ping -c 20 -W 1 pgm-che.eth.che.matuszyk.com
 
 . /root/bin/_script_footer.sh
