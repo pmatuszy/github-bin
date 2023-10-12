b8a665806a4eadec5420a151fff53e8c64dac918
root <matuszyk@matuszyk.com>
2023-10-12 21:04:19 +0200

    "new push from nuci7.eth.r.matuszyk.com @ 2023.10.12 21:04:19"

--- dstat.sh
+++ dstat.sh
@@ -15,9 +15,9 @@ fi
 clear 
 
 if (( $# == 0 ));then
- dstat -tcdnmg -D mmcblk0,nvme0n1,/dev/sd* -N ens33,eth0,eno1 -C total --top-cpu --top-io --top-mem --cpufreq -C total --bw --nocolor -f 1
+ dstat -tcdnmg -D mmcblk0,nvme0n1,/dev/sda -N ens33,eth0,eno1 -C total --top-cpu --top-io --top-mem --cpufreq -C total --bw --nocolor -f 1
 else
- dstat -tcdnmg -D mmcblk0,nvme0n1,/dev/sd* -N ens33,eth0,eno1 -C total --top-cpu --top-io --top-mem --cpufreq -C total --bw --nocolor --noupdate -f $1
+ dstat -tcdnmg -D mmcblk0,nvme0n1,/dev/sda -N ens33,eth0,eno1 -C total --top-cpu --top-io --top-mem --cpufreq -C total --bw --nocolor --noupdate -f $1
 fi
 
 . /root/bin/_script_footer.sh
