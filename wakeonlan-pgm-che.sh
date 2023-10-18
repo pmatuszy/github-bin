a47daebe3c5160efba2e6baefff66837b14257a5
root <pmatuszy.login+github@matuszyk.com>
2023-10-18 19:28:28 +0200

    "new push from nucvm.eth.che.matuszyk.com @ 2023.10.18 19:28:28"

--- wakeonlan-pgm-che.sh
+++ wakeonlan-pgm-che.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2023.10.18 - v. 0.4 - check if wake is needed (added ping at the beginning)
 # 2023.10.02 - v. 0.3 - added ping at the end
 # 2023.03.07 - v. 0.2 - added check for wakeonlan package
 # 2023.01.29 - v. 0.1 - initial release
@@ -9,13 +10,22 @@
 check_if_installed wakeonlan
 
 opoznienie=2
+IP=pgm-che.eth.che.matuszyk.com
+MAC="04:D9:F5:60:42:4A"
+
+ping -c 2  -W 2 -q "${IP}" >/dev/null
+
+if (( $? == 0 ));then
+  echo ; echo "(PGM) Host $IP is already up. No need to start it again...";echo
+  exit 0
+fi
 
 for p in {1..20};do 
-  wakeonlan -i 192.168.200.255 04:D9:F5:60:42:4A
+  wakeonlan -i 192.168.200.255 "$MAC"
   echo opoznienie $opoznienie
   sleep $opoznienie
 done
 
-ping -c 40 -W 1 pgm-che.eth.che.matuszyk.com
+ping -c 40 -W 1 $IP
 
 . /root/bin/_script_footer.sh
