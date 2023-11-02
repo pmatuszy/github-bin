6489001b1543d0f00989c10720761ad3d62e6f66
root <pmatuszy.login+github@matuszyk.com>
2023-11-02 19:05:07 +0100

    "new push from nucvm.eth.che.matuszyk.com @ 2023.11.02 19:05:07"

--- healthchecks-lvs-sync-status.sh
+++ healthchecks-lvs-sync-status.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2023.11.02 - v. 0.5 - bugfix: if no lvs present no error is returned
 # 2023.07.18 - v. 0.4 - bugfix: redirected 2> to stdout
 # 2023.02.28 - v. 0.3 - curl with kod_powrotu
 # 2023.01.03 - v. 0.2 - bug fixed - when no lvs'm the status should be ok
@@ -13,7 +14,10 @@ fi
 
 m=$( echo " "; echo "aktualna data: `date '+%Y.%m.%d %H:%M'`" ; echo ; 
      cat $0|grep -e '# *20[123][0-9]'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}' ; echo ; echo
-     lvs 2>&1
+     ile_vol=$(lvs 2>&1 | wc -l)
+     if (( $ile_vol == 0 ));then    # if there are no lvs we exit with no error code
+       exit 0
+     fi
 
      if [ $(lvs -o 'sync_percent' 2>&1 |sort|uniq|grep -v -e '^[[:space:]]*$'| grep -v 100.00|wc -l) -eq 1 ];then
        exit 0
