ff73de322cc42dec6c07b41b326a1a65205163f2
root <root@nuci7b.eth.che.matuszyk.com>
2023-11-03 13:31:58 +0100

    "new push from nuci7b.eth.che.matuszyk.com @ 2023.11.03 13:31:58"

--- HOST-SPECIFIC/nuci7b.eth.che.matuszyk.com/spr-pptp-asus-CHE.sh
+++ HOST-SPECIFIC/nuci7b.eth.che.matuszyk.com/spr-pptp-asus-CHE.sh
@@ -9,7 +9,7 @@
 
 export adres_publiczny_z_pl="31.179.173.42"
 export ROUTER_IP="192.168.200.230"
-export PAUSE_UP_TO_DATE="202311101"     # bez kropek YYYYMMDD
+export PAUSE_UP_TO_DATE="20231102"     # bez kropek YYYYMMDD
 
 if (( "$(date +%Y%m%d)" <= "${PAUSE_UP_TO_DATE}" ));then
   exit 0
