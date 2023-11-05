85f50b4dc25130bd504d379cdaa21212a532ee95
root <root@nuci7b.eth.che.matuszyk.com>
2023-11-05 21:03:01 +0100

    "new push from nuci7b.eth.che.matuszyk.com @ 2023.11.05 21:03:01"

--- apc-status.sh
+++ apc-status.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2023.11.05 - v. 0.3 - dodano sprawdzanie, czy pakiet apcupsd jest zainstalowany
 # 2023.01.16 - v. 0.2 - dodano random delay jesli skrypt jest wywolywany nieinteraktywnie
 # 2022.05.09 - v. 0.1 - initial release
 
@@ -8,6 +9,8 @@
 if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
 fi
+
+check_if_installed apcupsd
 
 # spr. czy nie ma bledow
 if [ $(/usr/bin/env apcaccess | egrep "STATUS   : ONLINE *$"|wc -l) -eq 0 ];then
