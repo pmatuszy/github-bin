205cdc26c634d316a8ab8a472720b61fe774ef28
root <root@TEMPLATE.COM>
2023-12-18 20:23:42 +0100

    "new push from vpn-che.eth.che.matuszyk.com @ 2023.12.18 20:23:42"

--- update-restic.sh
+++ update-restic.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2023.12.18 - v. 0.7 - bugfix: check if restic is installed
 # 2023.03.26 - v. 0.6 - added ile_prob i odstepy_miedzy_probami_sek
 # 2023.02.28 - v. 0.5 - curl with kod_powrotu
 # 2023.01.03 - v. 0.4 - dodano random delay jesli skrypt jest wywolywany nieinteraktywnie
