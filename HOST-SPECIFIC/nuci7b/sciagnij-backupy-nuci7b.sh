550d3e80e05f5df59a456c9ce6622428dc98fe4e
root <root@nuci7b.eth.che.matuszyk.com>
2023-12-20 17:57:44 +0100

    "new push from nuci7b.eth.che.matuszyk.com @ 2023.12.20 17:57:44"

--- HOST-SPECIFIC/nuci7b/sciagnij-backupy-nuci7b.sh
+++ HOST-SPECIFIC/nuci7b/sciagnij-backupy-nuci7b.sh
@@ -14,3 +14,11 @@ SKAD=cloud.eth.r.matuszyk.com:/var/www/202*bz2
 DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/cloud-var-www
 
 eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD
+
+
+export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-lublin-healthchecks
+SKAD=lublin.eth.r.matuszyk.com:/mnt/luks-RaidSonicB/postgres/backup/lublin.eth.r.matuszyk.com/pgbackrest/backup/healthchecks/*
+DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/postgresql-lublin
+
+eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD
+
