e8c027406b730a89b3eca2487b0497525a271b1c
root <pmatuszy.login+github@matuszyk.com>
2024-04-11 07:46:42 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2024.04.11 07:46:42"

--- HOST-SPECIFIC/backupche.eth.che.matuszyk.com/signal-daemon-log-TO-URUCHAMIAC.sh
+++ HOST-SPECIFIC/backupche.eth.che.matuszyk.com/signal-daemon-log-TO-URUCHAMIAC.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2024.04.11 - v. 0.4 - added --dbus as required by a new version of the daemon
 # 2023.02.02 - v. 0.3 - added --foreground option to be able to use Ctrl-C 
 # 2023.02.01 - v. 0.2 - added restart once a day
 # 202x.xx.xx - v. 0.1 - initial release (date unknown)
@@ -16,7 +17,7 @@ while : ; do
 
   echo "[`date '+%Y.%m.%d %H:%M:%S'`] restart signala"
   timeout --foreground --preserve-status --signal=HUP --kill-after=$((secs_nagrywania+120)) $((secs_nagrywania+60)) \
-       /opt/signal-cli/bin/signal-cli -u +41763691467 daemon 2>&1 > /encrypted/root/signal-output-`date '+%Y%m%d__%H_%M_%S'`.log
+       /opt/signal-cli/bin/signal-cli -u +41763691467 daemon --dbus 2>&1 > /encrypted/root/signal-output-`date '+%Y%m%d__%H_%M_%S'`.log
 
   sleep $opoznienie_miedzy_wywolaniami # opozniamy bo jak sa problemy z siecia, to by nie startowac od razu z nastepna proba...
   secs_to_midnight=$((($(date -d "tomorrow 00:00" +%s)-$(date +%s))))
