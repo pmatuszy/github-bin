4e0217bc1539c3e3ba8d1eee39768f01663e92fa
root <pmatuszy.login+github@matuszyk.com>
2024-12-03 18:24:49 +0100

    "new push from lublin.eth.r.matuszyk.com @ 2024.12.03 18:24:49"

--- HOST-SPECIFIC/lublin.eth.r.matuszyk.com/signal-cli-runme-lublin.sh
+++ HOST-SPECIFIC/lublin.eth.r.matuszyk.com/signal-cli-runme-lublin.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2024.12.03 - v. 0.2 - added sleep for signal not to restart too quickly
 # 2023.01.26 - v. 0.1 - initial release
 
 . /root/bin/_script_header.sh
@@ -14,6 +15,9 @@ echo
 
 ( sleep 10 ; for p in {1..60} ; do sleep 1 ; setfacl -m u:healthchecks:rw /tmp/signal-socket ; done ) 2>/dev/null &
 
-signal-cli -a +420739836207 daemon --socket /tmp/signal-socket
+while : ; do 
+  signal-cli -a +420739836207 daemon --socket /tmp/signal-socket
+  sleep 15    # do not restart too often
+done
 
 . /root/bin/_script_footer.sh
