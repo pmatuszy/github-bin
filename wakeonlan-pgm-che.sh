386d818f919c66c78a3ec576528cb6d5f21e560c
root <pmatuszy.login+github@matuszyk.com>
2025-04-25 08:21:36 +0200

    "new push from backupche.eth.che.matuszyk.com @ 2025.04.25 08:21:36"

--- wakeonlan-pgm-che.sh
+++ wakeonlan-pgm-che.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2025.04.25 - v. 0.5 - added some cosmetic improvements to the script according to ChatGPT suggestions
 # 2023.10.18 - v. 0.4 - check if wake is needed (added ping at the beginning)
 # 2023.10.02 - v. 0.3 - added ping at the end
 # 2023.03.07 - v. 0.2 - added check for wakeonlan package
@@ -9,9 +10,10 @@
 
 check_if_installed wakeonlan
 
-opoznienie=2
+delay=2
 IP=pgm-che.eth.che.matuszyk.com
 MAC="04:D9:F5:60:42:4A"
+BROADCAST="192.168.200.255"
 
 ping -c 2  -W 2 -q "${IP}" >/dev/null
 
@@ -21,11 +23,17 @@ if (( $? == 0 ));then
 fi
 
 for p in {1..20};do 
-  wakeonlan -i 192.168.200.255 "$MAC"
-  echo opoznienie $opoznienie
-  sleep $opoznienie
+  wakeonlan -i "$BROADCAST" "$MAC"
+  echo delay $delay
+  sleep $delay
 done
 
-ping -c 40 -W 1 $IP
+if ping -c 3 -W 1 "$IP" > /dev/null; then
+  echo "(PGM) Host $IP successfully woke up."
+else
+  echo "(PGM) Host $IP did not respond after WOL attempts."
+fi
+
+logger "WOL script: sent packet to $MAC at $BROADCAST"
 
 . /root/bin/_script_footer.sh
