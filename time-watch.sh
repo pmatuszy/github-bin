7d74e4e049241879d97d59e17e009db9854f7b2b
root <matuszyk@matuszyk.com>
2025-05-20 17:33:49 +0200

    "new push from nuci7.eth.r.matuszyk.com @ 2025.05.20 17:33:49"

---
+++ time-watch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 2023.01.16 - v. 0.1 - initial release
+
+. /root/bin/_script_header.sh
+
+check_if_installed figlet watch
+
+#watch -t -n1 "date '+%Y.%m.%d %H:%M:%S'|figlet -f banner"
+watch -t -n1 "date '+%Y.%m.%d %H:%M:%S' | figlet -f big"
+
+. /root/bin/_script_footer.sh
