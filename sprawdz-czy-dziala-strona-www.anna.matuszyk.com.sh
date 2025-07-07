9c63516abb8a457ba163d344502bc6d5bb74fdf8
root <matuszyk@matuszyk.com>
2025-07-07 08:30:14 +0200

    "new push from nuci7.eth.r.matuszyk.com @ 2025.07.07 08:30:14"

--- sprawdz-czy-dziala-strona-www.anna.matuszyk.com.sh
+++ sprawdz-czy-dziala-strona-www.anna.matuszyk.com.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# 2024.03.12 - v. 0.3 - bugfix release
-# 2024.03.12 - v. 0.2 - added how_many_retries and retry_delay
-# 2023.02.16 - v. 0.1 - initial release
+# 2025.07.06 - v. 0.8 - bugfix - how_many_retries was not decremented... so script was running sometimes forever
+# 2024.04.02 - v. 0.7 - added timeout command (as curl sometimes doesn't timeout )
+# 2023.04.13 - v. 0.6 - added how_many_retries and retry_delay
+# 2023.01.17 - v. 0.5 - dodano random delay jesli skrypt jest wywolywany nieinteraktywnie
+# 2022.05.16 - v. 0.3 - commented out sending emails sections
+# 2022.05.03 - v. 0.2 - added healthcheck support
+# 2021.xx.xx - v. 0.1 - initial release
 
 . /root/bin/_script_header.sh
 
@@ -10,23 +14,33 @@ if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
 fi
 
-URL=www.anna.matuszyk.com  ; export URL
+export URL="www.anna.matuszyk.com"  ; export URL
 blad=1
 how_many_retries=10
 retry_delay=15
 
+curl_retry=1
+curl_retry_delay=3
+
+export timeout=20
+export kill_after=30
+
 while (( $blad != 0 && $how_many_retries != 0 )) ; do
-  if [ $(wget $URL -qO - |grep "In Short"|wc -l) -gt 0 ];then 
-    /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 -o /dev/null "$HEALTHCHECK_URL" 2>/dev/nulla
+  if [ $(/usr/bin/timeout --foreground --preserve-status --kill-after=$kill_after $timeout wget $URL -qO - |grep "Directory Listing"|wc -l) -gt 0 ];then
+    /usr/bin/timeout --foreground --preserve-status --kill-after=$kill_after $timeout /usr/bin/curl -fsS -m 100 --retry $curl_retry --retry-delay $curl_retry_null "$HEALTHCHECK_URL" 2>/dev/null
     blad=0
     break
-  else 
+  else
     sleep $retry_delay
+    ((how_many_retries--))
+    if (( script_is_run_interactively == 1 ));then
+       echo sleeping for $retry_delay
+    fi
   fi
 done
 
 if (( $blad != 0 ));then
-   /usr/bin/curl -fsS -m 100 --retry 10 --retry-delay 10 -o /dev/null "$HEALTHCHECK_URL"/fail 2>/dev/null
+   /usr/bin/timeout --foreground --preserve-status --kill-after=$kill_after $timeout /usr/bin/curl -fsS -m 100 --retry $how_many_retries  --retry-delay $retry/null "$HEALTHCHECK_URL"/fail 2>/dev/null
 fi
 
 . /root/bin/_script_footer.sh
@@ -36,4 +50,4 @@ exit $?
 #####
 # new crontab entry
 
-*/15 * * * * /root/bin/sprawdz-czy-dziala-strona-www.anna.matuszyk.com.sh
+*/15 * * * *  /usr/bin/flock --nonblock --exclusive /root/bin/sprawdz-czy-dziala-strona-www.anna.matuszyk.com.sh -c /root/bin/sprawdz-czy-dziala-strona-www.anna.matuszyk.com.sh
