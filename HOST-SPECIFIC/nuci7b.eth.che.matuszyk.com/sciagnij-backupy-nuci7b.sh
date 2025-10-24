973c35505585ab2e75fffeee39a644836a7a3e8d
root <root@nuci7b.eth.che.matuszyk.com>
2025-10-24 20:11:24 +0200

    "new push from nuci7b.eth.che.matuszyk.com @ 2025.10.24 20:11:24"

--- HOST-SPECIFIC/nuci7b.eth.che.matuszyk.com/sciagnij-backupy-nuci7b.sh
+++ HOST-SPECIFIC/nuci7b.eth.che.matuszyk.com/sciagnij-backupy-nuci7b.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2025.10.24 - v. 0.3 - bugfix: "bash: line 1: /usr/bin/rsync: Argument list too long" - in SKAD is in double quotes and end changed to /* to /
 # 2023.12.20 - v. 0.2 - added healthchecks download
 # 2023.03.14 - v. 0.1 - initial release
 
@@ -13,22 +14,22 @@ eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD
 
 export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-cloud
 SKAD=cloud.eth.r.matuszyk.com:/var/www/202*bz2
-DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/cloud-var-www
+DOKAD="/mnt/luks-buffalo2/_backupy-1dyne_kopie/cloud-var-www"
 
 eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD
 
 
 
 export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-lublin-healthchecks-logs
-SKAD=lublin.eth.r.matuszyk.com:/mnt/luks-RaidSonicB/postgres/backup/lublin.eth.r.matuszyk.com/pgbackrest/logs/*
-DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/postgresql-lublin/healthchecks-pgbackreset-logs/
+SKAD="lublin.eth.r.matuszyk.com:/mnt/luks-RaidSonicB/postgres/backup/lublin.eth.r.matuszyk.com/pgbackrest/logs/"
+DOKAD="/mnt/luks-buffalo2/_backupy-1dyne_kopie/postgresql-lublin/healthchecks-pgbackreset-logs/"
 
 eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD
 
 
 
 export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-lublin-healthchecks-backups
-SKAD=lublin.eth.r.matuszyk.com:/mnt/luks-RaidSonicB/postgres/backup/lublin.eth.r.matuszyk.com/pgbackrest/backup/healthchecks/*
-DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/postgresql-lublin/healthchecks-backupy/
+SKAD="lublin.eth.r.matuszyk.com:/mnt/luks-RaidSonicB/postgres/backup/lublin.eth.r.matuszyk.com/pgbackrest/backup/healthchecks/"
+DOKAD="/mnt/luks-buffalo2/_backupy-1dyne_kopie/postgresql-lublin/healthchecks-backupy/"
 
 eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD
