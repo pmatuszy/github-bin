b9f0214c7375af9ed261c10972d79f3d831d7afd
root <root@nuci7b.eth.che.matuszyk.com>
2025-10-24 20:13:13 +0200

    "new push from nuci7b.eth.che.matuszyk.com @ 2025.10.24 20:13:13"

--- HOST-SPECIFIC/nuci7b.eth.che.matuszyk.com/sciagnij-backupy-nuci7b.sh
+++ HOST-SPECIFIC/nuci7b.eth.che.matuszyk.com/sciagnij-backupy-nuci7b.sh
@@ -8,7 +8,7 @@ export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-www02
 SKAD=www02.eth.r.matuszyk.com:/var/www/202*bz2
 DOKAD=/mnt/luks-buffalo2/_backupy-1dyne_kopie/www02
 
-eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD
+eval /root/bin/sciagnij-backupy.sh "$SKAD" "$DOKAD"
 
 
 
@@ -16,7 +16,7 @@ export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-cloud
 SKAD=cloud.eth.r.matuszyk.com:/var/www/202*bz2
 DOKAD="/mnt/luks-buffalo2/_backupy-1dyne_kopie/cloud-var-www"
 
-eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD
+eval /root/bin/sciagnij-backupy.sh "$SKAD" "$DOKAD"
 
 
 
@@ -24,7 +24,7 @@ export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-lublin-healthchecks-logs
 SKAD="lublin.eth.r.matuszyk.com:/mnt/luks-RaidSonicB/postgres/backup/lublin.eth.r.matuszyk.com/pgbackrest/logs/"
 DOKAD="/mnt/luks-buffalo2/_backupy-1dyne_kopie/postgresql-lublin/healthchecks-pgbackreset-logs/"
 
-eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD
+eval /root/bin/sciagnij-backupy.sh "$SKAD" "$DOKAD"
 
 
 
@@ -32,4 +32,4 @@ export HEALTHCHECKS_FORCE_ID=sciagnij-backupy.sh-lublin-healthchecks-backups
 SKAD="lublin.eth.r.matuszyk.com:/mnt/luks-RaidSonicB/postgres/backup/lublin.eth.r.matuszyk.com/pgbackrest/backup/healthchecks/"
 DOKAD="/mnt/luks-buffalo2/_backupy-1dyne_kopie/postgresql-lublin/healthchecks-backupy/"
 
-eval /root/bin/sciagnij-backupy.sh $SKAD $DOKAD
+eval /root/bin/sciagnij-backupy.sh "$SKAD" "$DOKAD"
