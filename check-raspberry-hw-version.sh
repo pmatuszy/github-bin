10f0932db39188ab927154fbe8f754c6ae0a9d2e
root <pmatuszy.login+github@matuszyk.com>
2025-10-27 10:10:20 +0100

    "new push from rpir2.eth.r.matuszyk.com @ 2025.10.27 10:10:20"

--- check-raspberry-hw-version.sh
+++ check-raspberry-hw-version.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# 2025.10.27 - v. 0.3 - bugfix: if mem < 1GB it was not properly formatted and displayed 0 GB - ChatGPT helped on that one
 # 2020.12.08 - v. 0.2 - adding info about total RAM
 # 2020.0x.xx - v. 0.1 - initial release (date unknown)
 
 # tr -d '\0'` below is to get rid of the message "warning: command substitution: ignored null byte in input"
 
 echo
-echo `cat /proc/device-tree/model|tr -d '\0'` ",   " `cat /proc/meminfo |grep MemTotal|awk '{printf ("Total RAM: %.0f GB", $2/1024/1024)}'`
+echo "$(tr -d '\0' </proc/device-tree/model),   $(awk '/MemTotal/ {printf "Total RAM: %.2f GB", $2/1024/1024}' /proc/meminfo)"
+#echo `cat /proc/device-tree/model|tr -d '\0'` ",   " `cat /proc/meminfo |grep MemTotal|awk '{printf ("Total RAM: %.0f GB", $2/1024/1024)}'`
 echo
-
