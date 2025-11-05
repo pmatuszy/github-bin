37079a765adbd20a90dd3c2317c6caefa7e080c4
root <root@nuci7b.eth.che.matuszyk.com>
2025-11-05 10:49:46 +0100

    "new push from nuci7b.eth.che.matuszyk.com @ 2025.11.05 10:49:46"

--- healthchecks-lvs-sync-status.sh
+++ healthchecks-lvs-sync-status.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 2025.11.05 - v. 0.6 - added info about pvs, vgs, lvs
 # 2023.11.02 - v. 0.5 - bugfix: if no lvs present no error is returned
 # 2023.07.18 - v. 0.4 - bugfix: redirected 2> to stdout
 # 2023.02.28 - v. 0.3 - curl with kod_powrotu
@@ -12,14 +13,32 @@ if [ -f "$HEALTHCHECKS_FILE" ];then
   HEALTHCHECK_URL=$(cat "$HEALTHCHECKS_FILE" |grep "^`basename $0`"|awk '{print $2}')
 fi
 
-m=$( echo " "; echo "aktualna data: `date '+%Y.%m.%d %H:%M'`" ; echo ; 
-     cat $0|grep -e '# *20[123][0-9]'|head -n 1 | awk '{print "script version: " $5 " (dated "$2")"}' ; echo ; echo
-     ile_vol=$(lvs 2>&1 | wc -l)
+m=$( echo "${SCRIPT_VERSION}";echo
+     ile_vol=$(lvs 2>&1 | grep -v LSize | wc -l)
      if (( $ile_vol == 0 ));then    # if there are no lvs we exit with no error code
+       lvs --segments
        exit 0
      fi
 
      if [ $(lvs -o 'sync_percent' 2>&1 |sort|uniq|grep -v -e '^[[:space:]]*$'| grep -v 100.00|wc -l) -eq 1 ];then
+       boxes <<< "lvs " ;  echo
+       lvs 
+       echo
+       boxes <<< "lvs --segments" ;  echo 
+       lvs --segments
+       echo
+
+       boxes <<< "vgs" ;  echo
+       vgs
+       echo 
+
+       boxes <<< "pvs" ;  echo
+       pvs
+       echo
+
+       boxes <<< "pvs --segments -v" ;  echo
+       pvs --segments -v
+
        exit 0
      else
        exit 1
