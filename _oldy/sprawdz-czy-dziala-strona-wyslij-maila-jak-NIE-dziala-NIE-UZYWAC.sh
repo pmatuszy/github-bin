7dcddc123c1da90baf6cd5f45397e7bb039ed832
root <pmatuszy.login+github@matuszyk.com>
2026-07-17 21:55:53 +0200

    new push from secureuploadserver.eth.r.matuszyk.com @ 2026.07.17 21:55:53

--- _oldy/sprawdz-czy-dziala-strona-wyslij-maila-jak-NIE-dziala-NIE-UZYWAC.sh
+++
@@ -1,3 +0,0 @@
-if [ `wget yt2podcast.com:8080 -qO - |grep .xml|wc -l` -eq 0 ];then 
-   echo "!!! yt2podcast.com:8080 site is DOWN" | strings | aha | /usr/bin/mailx -r root@`hostname` -a 'Content-Type: text/html' -s "(`hostname --short`) PROBLEM - yt2podcast.com:8080 site is DOWN" matuszyk+`hostname`@matuszyk.com
-fi
